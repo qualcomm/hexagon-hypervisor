29439bd4e54d91e7334043dbb7aea33072a6de05
Richard Kuo <rkuo@qti.qualcomm.com>
2009-11-13 23:33:48 +0000

    fix extension import
    Signed-off-by: Richard Kuo <rkuo@qti.qualcomm.com>

--- scripts/docs/conf.py
+++ scripts/docs/conf.py
@@ -27,7 +27,7 @@ sys.path.append(os.path.abspath('scripts/docs'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo','sphinx.ext.blast']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo','blast']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['scripts/docs/_templates']

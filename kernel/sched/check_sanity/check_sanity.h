4765c53a901144df783950b41ec34846c669b0b5
Richard Kuo <rkuo@qti.qualcomm.com>
2009-11-18 18:30:51 +0000

    debug needs to come after the prototypes
    Signed-off-by: Richard Kuo <rkuo@qti.qualcomm.com>

--- kernel/sched/check_sanity/check_sanity.h
+++ kernel/sched/check_sanity/check_sanity.h
@@ -7,11 +7,12 @@
 #define BLASTK_CHECK_SANITY_H 1
 
 #include <c_std.h>
-#ifdef DEBUG
-#include <check_sanity_debug.h>
-#endif
 
 u64_t BLASTK_check_sanity(const u64_t retval);
 u64_t BLASTK_check_sanity_unlock(const u64_t retval);
+
+#ifdef DEBUG
+#include <check_sanity_debug.h>
+#endif
 
 #endif

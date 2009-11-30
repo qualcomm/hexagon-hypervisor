f76c9934415f6a2e3af2ac8ca9398b3989fba008
Erich Plondke <erich@qti.qualcomm.com>
2009-11-30 01:53:12 +0000

    make changing my own priority work, at least...
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

--- kernel/traps/prio/prio.ref.c
+++ kernel/traps/prio/prio.ref.c
@@ -4,10 +4,22 @@
  */
 
 #include <prio.h>
+#include <runlist.h>
 
 u32_t BLASTK_prio_set(BLASTK_thread_context *dest, u32_t prio, BLASTK_thread_context *me)
 {
-	return me->prio;
+	u32_t ret;
+	if (prio > MAX_PRIOS) prio = MAX_PRIOS;
+	if (dest == me) {
+		BLASTK_runlist_remove(me);
+		ret = me->prio;
+		me->prio = prio;
+		BLASTK_runlist_push(me);
+	} else {
+		/* UNIMPLEMENTED */
+		ret = prio;
+	}
+	return ret;
 }
 
 u32_t BLASTK_prio_get(BLASTK_thread_context *me)

4ad51499aa3dd4b15918894684e2ac64e55cbc34
Erich Plondke <erich@qti.qualcomm.com>
2010-01-05 22:38:20 +0000

    tests for interrupt configuration
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

---
+++ kernel/data/intconfig/test/test.c
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#include <c_std.h>
+#include <asm_offsets.h>
+#include <context.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <intconfig.h>
+#include <setjmp.h>
+#include <max.h>
+#include <resched.h>
+
+#define BAD ((void *)(0xdeadbeef))
+
+#define TEST_FASTINT_TRAPMASK 0x9
+
+H2K_thread_context a;
+
+void FAIL(const char *str)
+{
+	puts("FAIL");
+	puts(str);
+	exit(1);
+}
+
+void TH_handler(u32_t x)
+{
+}
+
+void H2K_fastint();
+
+int main() 
+{
+	int i;
+	u32_t oldmask;
+	a.ugpgp = 0x12345678F0000000ULL;
+	for (i = 0; i < MAX_INTERRUPTS; i++) {
+		H2K_fastint_funcptrs[i] = BAD;
+		H2K_inthandlers[i] = BAD;
+		H2K_fastint_mask = 0xDEADBEEFU;
+	}
+	for (i = 0; i < MAX_HTHREADS; i++) {
+		H2K_fastint_contexts[i].context.r0100 = 0xdeadbeefcafebabeULL;
+		H2K_fastint_contexts[i].context.hthread = 0xf;
+		H2K_fastint_contexts[i].context.trapmask = 0xdead;
+	}
+	H2K_intconfig_init();
+	for (i = 0; i < MAX_INTERRUPTS; i++) {
+		if (i != RESCHED_INT) {
+			if (H2K_inthandlers[i] != NULL) FAIL("uninitialized handler");
+		} else {
+			if (H2K_inthandlers[i] != H2K_resched) FAIL("wrong resched handler");
+		}
+		if (H2K_fastint_funcptrs[i] != NULL) FAIL("uninitialized fastint ptr");
+	}
+	for (i = 0; i < MAX_HTHREADS; i++) {
+		if (H2K_fastint_contexts[i].context.r0100) FAIL("Uninitialized fastint context");
+		if (H2K_fastint_contexts[i].context.hthread != i) FAIL("Uninitialized fastint context");
+		if (H2K_fastint_contexts[i].context.trapmask != TEST_FASTINT_TRAPMASK) FAIL("bad trapmask");
+	}
+	if (H2K_fastint_mask) FAIL("uninitialized fastint mask");
+	for (i = 0; i < MAX_INTERRUPTS; i++) {
+		if (i == RESCHED_INT) continue;
+		oldmask = H2K_fastint_mask;
+		if ((1<<i) & oldmask) FAIL("fastint bit already set");
+		H2K_register_fastint(i,TH_handler,&a);
+		if (((1<<i) | oldmask) != H2K_fastint_mask) FAIL("fastint mask bit not set");
+		if (H2K_inthandlers[i] != H2K_fastint) FAIL("fastint handler not set");
+		if (H2K_fastint_funcptrs[i] != TH_handler) FAIL("wrong handler func");
+		if (H2K_fastint_gp != 0xF0000000U) FAIL("fastint gp not set");
+	}
+	puts("PASS");
+	return 0;
+}
+

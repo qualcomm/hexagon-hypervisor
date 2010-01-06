2ab7ed04d2878486e3cc71bdf1386cd25f46ae9f
Erich Plondke <erich@qti.qualcomm.com>
2010-01-06 19:17:47 +0000

    needs full setup
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

---
+++ kernel/init/boot/test/test.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#include <c_std.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void h2_init();
+
+void FAIL(const char *str)
+{
+	puts("FAIL");
+	puts(str);
+	exit(1);
+}
+
+int main()
+{
+	u32_t tmp,i;
+	h2_init();
+	for (i = 0; i < 1000; i++) {
+		h2_init();
+	}
+	asm ( " %0 = modectl " :"=r"(tmp));
+	if ((tmp & 0x00fe) == 0) FAIL("Didn't start up other threads");
+	asm ( " %0 = syscfg " : "=r"(tmp));
+	if ((tmp & 7) != 7) FAIL("Didn't start up syscfg correctly");
+	puts("PASS");
+	return 0;
+}
+

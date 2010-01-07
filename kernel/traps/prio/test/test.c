8176109552db32153fcfbb86577cc6c281969e62
Erich Plondke <erich@qti.qualcomm.com>
2010-01-07 23:35:45 +0000

    tests for priority
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

---
+++ kernel/traps/prio/test/test.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#include <c_std.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <context.h>
+#include <prio.h>
+#include <max.h>
+
+void FAIL(const char *str)
+{
+	puts("FAIL");
+	puts(str);
+	exit(1);
+}
+
+H2K_thread_context a;
+
+int main()
+{
+	u32_t i;
+	for (i = 0; i < MAX_PRIOS; i++) {
+		a.prio = i;
+		if (H2K_prio_get(&a) != i) FAIL("prio_get");
+	}
+	if (H2K_prio_set(&a,4,&a) != -1) FAIL("prio_set");
+	puts("PASS");
+	return 0;
+}
+

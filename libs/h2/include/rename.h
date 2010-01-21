4063a2a575df93fc604424342ebafd7f31ce1122
Richard Kuo <rkuo@qti.qualcomm.com>
2010-01-21 00:48:47 +0000

    rearranged and added all blast_*.h contents; will probably need to pull out what doesn't actually exist or make sense.
    Signed-off-by: Richard Kuo <rkuo@qti.qualcomm.com>

--- libs/h2/include/rename.h
+++ libs/h2/include/rename.h
@@ -14,88 +14,32 @@
 
 #define REDEF_FUNCTION(RET,NAME,ARGS,CALLARGS) static inline RET RENAME_PREFIX##_##NAME ARGS { return h2_##NAME CALLARGS ; }
 
-/* "sos_alloc.h" */
+/*  "blast_alloc.h"  */
 
 REDEF_FUNCTION(void *,malloc,(unsigned int size),(size));
 REDEF_FUNCTION(void *,calloc,(unsigned int elsize, unsigned int num),(elsize,num));
 REDEF_FUNCTION(void *,realloc,(void *ptr, int newsize),(ptr,newsize));
 REDEF_FUNCTION(void ,free,(void *ptr),(ptr));
 
-// #include "sos_futex.h"
+/*  "blast_allsignal.h"  */
 
-REDEF_FUNCTION(int ,futex_wait,(void *lock, int val),(lock,val));
-REDEF_FUNCTION(int ,futex_wake,(void *lock, int n_to_wake),(lock,n_to_wake));
+typedef h2_allsignal_t blast_allsignal_t;
+REDEF_FUNCTION(void ,allsignal_init,(h2_allsignal_t *signal),(signal));
+REDEF_FUNCTION(void ,allsignal_wait,(h2_allsignal_t *signal, unsigned int mask),(signal,mask));
+REDEF_FUNCTION(void ,allsignal_signal,(h2_allsignal_t *signal, unsigned int mask),(signal,mask));
 
-// #include "sos_intwait.h"
+/*  "blast_anysignal.h"  */
 
-REDEF_FUNCTION(int ,intwait,(unsigned long long int mask),(mask));
+typedef h2_anysignal_t blast_anysignal_t;
+REDEF_FUNCTION(void ,anysignal_init,(h2_anysignal_t *signal),(signal));
+REDEF_FUNCTION(void ,anysignal_wait,(h2_anysignal_t *signal, unsigned int mask),(signal,mask));
+REDEF_FUNCTION(unsigned int ,anysignal_set,(h2_anysignal_t *signal, unsigned int mask),(signal,mask));
+REDEF_FUNCTION(unsigned int ,anysignal_get,(h2_anysignal_t *signal),(signal));
+REDEF_FUNCTION(unsigned int ,anysignal_clear,(h2_anysignal_t *signal, unsigned int mask),(signal,mask));
 
-// #include "sos_mutex.h"
+/*  "blast_barrier.h"  */
 
-typedef h2_mutex_t sos_mutex_t;
-
-REDEF_FUNCTION(void ,mutex_lock,(h2_mutex_t *lock),(lock));		/* blocking */
-REDEF_FUNCTION(void ,mutex_unlock,(h2_mutex_t *lock),(lock));	/* unlock */
-REDEF_FUNCTION(int ,mutex_trylock,(h2_mutex_t *lock),(lock));	/* just try... 1 if successful, 0 if not */
-REDEF_FUNCTION(void ,mutex_init,(h2_mutex_t *lock),(lock)) 	/* initialize it... */
-
-// #include "sos_pipe.h"
-
-typedef h2_pipe_data_t sos_pipe_data_t;
-
-typedef h2_pipe_t sos_pipe_t;
-
-REDEF_FUNCTION(h2_pipe_t *,pipe_alloc,(unsigned int size_in_bytes),(size_in_bytes));
-REDEF_FUNCTION(h2_pipe_t *,pipe_create,(h2_pipe_t *pipe, h2_pipe_data_t *data, int data_elements),(pipe,data,data_elements));
-REDEF_FUNCTION(void ,pipe_free,(h2_pipe_t *pipe),(pipe));
-
-REDEF_FUNCTION(void ,pipe_send,(h2_pipe_t *pipe, h2_pipe_data_t data),(pipe,data));
-REDEF_FUNCTION(h2_pipe_data_t, pipe_recv,(h2_pipe_t *pipe),(pipe));
-
-REDEF_FUNCTION(int ,pipe_trysend,(h2_pipe_t *pipe, h2_pipe_data_t data),(pipe,data));
-REDEF_FUNCTION(h2_pipe_data_t, pipe_tryrecv,(h2_pipe_t *pipe, int *success),(pipe,success));
-
-// #include "sos_printf.h"
-
-// REDEF_FUNCTION(int ,printf,(const char *fmt, ...));
-#define sos_printf(...) h2_printf(__VA_ARGS__)
-
-// #include "sos_thread.h"
-
-REDEF_FUNCTION(int ,thread_create,(void *pc, void *stack, void *arg, unsigned int prio, unsigned int asid),(pc,stack,arg,prio,asid));
-REDEF_FUNCTION(void ,thread_stop,(),());
-REDEF_FUNCTION(int ,thread_myid,(),());
-
-// #include "sos_trace.h"
-
-#ifndef SOS_DEBUG
-#define SOS_TRACE(str, ...) __VA_ARGS__
-#else
-#define SOS_TRACE(str, ...) \
-	do { \
-		h2_printf("%s:%d: %s: >>> calling %s\n",__FILE__,__LINE__,str,#__VA_ARGS__); \
-		__VA_ARGS__; \
-		h2_printf("%s:%d: %s: <<< %s returned\n",__FILE__,__LINE__,str,#__VA_ARGS__); \
-	} while (0);
-#endif
-
-// #include "sos_cycles.h"
-REDEF_FUNCTION(unsigned long long int ,get_pcycles,(),());
-
-REDEF_FUNCTION(unsigned long long int ,get_tcycles,(),());
-
-// #include "sos_sem.h"
-typedef h2_sem_t sos_sem_t;
-
-REDEF_FUNCTION(int ,sem_add,(h2_sem_t *sem, unsigned int amt),(sem,amt));
-REDEF_FUNCTION(int ,sem_up,(h2_sem_t *sem),(sem));
-REDEF_FUNCTION(int ,sem_down,(h2_sem_t *sem),(sem));
-REDEF_FUNCTION(int ,sem_trydown,(h2_sem_t *sem),(sem));
-REDEF_FUNCTION(void ,sem_init,(h2_sem_t *sem),(sem));
-
-// #include "sos_barrier.h"
-
-typedef h2_barrier_t sos_barrier_t;
+typedef h2_barrier_t blast_barrier_t;
 
 #define H2_BARRIER_SERIAL_THREAD 1
 #define H2_BARRIER_OTHER 0
@@ -103,21 +47,129 @@ typedef h2_barrier_t sos_barrier_t;
 REDEF_FUNCTION(int ,barrier_init,(h2_barrier_t *barrier, unsigned int threads_total),(barrier,threads_total));
 REDEF_FUNCTION(int ,barrier_wait,(h2_barrier_t *barrier),(barrier));
 
-// #include "sos_libkernel.h"
-REDEF_FUNCTION(void ,init,(unsigned long long int *memmap),(memmap));
-// #include "sos_fastint.h"
+/*  "blast_cond.h"  */
+
+typedef h2_cond_t blast_cond_t;
+
+REDEF_FUNCTION(void ,cond_signal,(h2_cond_t *cond),(cond));
+REDEF_FUNCTION(void ,cond_broadcast,(h2_cond_t *cond),(cond));
+REDEF_FUNCTION(void ,cond_wait,(h2_cond_t *cond,h2_mutex_t *mutex),(cond,mutex));
+REDEF_FUNCTION(void ,cond_init,(h2_cond_t *cond),(cond));
+
+/*  "blast_config.h"  */
+
+REDEF_FUNCTION(void ,config_add_thread_storage,(void *buf, unsigned int size),(buf,size));
+
+/*  "blast_cycles.h"  */
+
+REDEF_FUNCTION(unsigned long long int ,get_pcycles,(void),());
+REDEF_FUNCTION(unsigned long long int ,get_tcycles,(void),());
+REDEF_FUNCTION(unsigned long long int ,get_core_pcycles,(void),());
+
+/*  "blast_fastint.h"  */
+
 REDEF_FUNCTION(void ,register_fastint,(int intno, void (*fn)(int)),(intno,fn));
 
-// #include "sos_allsignal.h"
+/*  "blast_futex.h"  */
 
-typedef h2_allsignal_t sos_allsignal_t;
-REDEF_FUNCTION(void ,allsignal_init,(h2_allsignal_t *signal),(signal));
-REDEF_FUNCTION(void ,allsignal_wait,(h2_allsignal_t *signal, unsigned int mask),(signal,mask));
-REDEF_FUNCTION(void ,allsignal_signal,(h2_allsignal_t *signal, unsigned int mask),(signal,mask));
+REDEF_FUNCTION(int ,futex_wait,(void *lock, int val),(lock,val));
+REDEF_FUNCTION(int ,futex_wake,(void *lock, int n_to_wake),(lock,n_to_wake));
 
-typedef h2_anysignal_t sos_anysignal_t;
-REDEF_FUNCTION(void ,anysignal_init,(h2_anysignal_t *signal),(signal));
-REDEF_FUNCTION(void ,anysignal_wait,(h2_anysignal_t *signal, unsigned int mask),(signal,mask));
-REDEF_FUNCTION(void ,anysignal_signal,(h2_anysignal_t *signal, unsigned int mask),(signal,mask));
+/*  "blast_intwait.h"  */
+
+REDEF_FUNCTION(int ,intwait,(unsigned long long int mask),(mask));
+
+/*  "blast_libkernel.h  */
+
+#define H2_PREINIT_L2CACHE_SIZE_0K 0
+#define H2_PREINIT_L2CACHE_SIZE_64K 1
+#define H2_PREINIT_L2CACHE_SIZE_128K 2
+#define H2_PREINIT_L2CACHE_SIZE_256K 3
+
+REDEF_FUNCTION(void ,init,(unsigned long long int *memmap),(memmap));
+REDEF_FUNCTION(void ,preinit_hthread_startup,(unsigned int mask),(mask));
+REDEF_FUNCTION(void ,preinit_l2cache_size,(int size),(size));
+REDEF_FUNCTION(void ,preinit_relocate_tcm,(void *addr),(addr));
+
+/*  "blast_mutex.h"  */
+
+typedef h2_mutex_t blast_mutex_t;
+
+REDEF_FUNCTION(void ,mutex_lock,(h2_mutex_t *lock),(lock));		/* blocking */
+REDEF_FUNCTION(void ,mutex_unlock,(h2_mutex_t *lock),(lock));	/* unlock */
+REDEF_FUNCTION(int ,mutex_trylock,(h2_mutex_t *lock),(lock));	/* just try... 1 if successful, 0 if not */
+REDEF_FUNCTION(void ,mutex_init,(h2_mutex_t *lock),(lock)) 	/* initialize it... */
+
+/*  "blast_pipe.h"  */
+
+typedef h2_pipe_data_t blast_pipe_data_t;
+typedef h2_pipe_t blast_pipe_t;
+
+REDEF_FUNCTION(h2_pipe_t *,pipe_alloc,(unsigned int size_in_bytes),(size_in_bytes));
+REDEF_FUNCTION(h2_pipe_t *,pipe_create,(h2_pipe_t *pipe, h2_pipe_data_t *data, int data_elements),(pipe,data,data_elements));
+REDEF_FUNCTION(void ,pipe_free,(h2_pipe_t *pipe),(pipe));
+
+REDEF_FUNCTION(void ,pipe_send,(h2_pipe_t *pipe, h2_pipe_data_t data),(pipe,data));
+REDEF_FUNCTION(h2_pipe_data_t , pipe_recv,(h2_pipe_t *pipe),(pipe));
+
+REDEF_FUNCTION(int ,pipe_trysend,(h2_pipe_t *pipe, h2_pipe_data_t data),(pipe,data));
+REDEF_FUNCTION(h2_pipe_data_t , pipe_tryrecv,(h2_pipe_t *pipe, int *success),(pipe,success));
+
+/*  "blast_prefetch.h"  */
+
+#define H2_PREFETCH_I 1
+#define H2_PREFETCH_D 2
+#define H2_PREFETCH_SW 4
+
+REDEF_FUNCION(void ,h2_set_prefetch,(unsigned int settings),(settings));
+
+/*  "blast_printf.h"  */
+
+#define blast_printf(...) h2printf(__VA_ARGS__)
+
+/*  "blast_prio.h"  */
+
+REDEF_FUNCTION(int ,get_prio,(void),(void));
+REDEF_FUNCTION(int ,set_prio,(unsigned int threadid, unsigned int newprio),(threadid,newprio));
+REDEF_FUNCTION(unsigned int ,mask_prios_above,(unsigned int worst_prio),(worst_prio));
+
+/*  "blast_rmutex.h"  */
+
+typedef h2_rmutex_t blast_rmutex_t;
+
+REDEF_FUNCTION(void ,rmutex_lock,(h2_rmutex_t *lock),(lock));
+REDEF_FUNCTION(void ,rmutex_unlock,(h2_rmutex_t *lock),(lock));
+REDEF_FUNCTION(int ,rmutex_trylock,(h2_rmutex_t *lock),(lock));
+REDEF_FUNCTION(void ,rmutex_init,(h2_rmutex_t *lock),(lock));
+
+/*  "blast_sem.h"  */
+typedef h2_sem_t blast_sem_t;
+
+REDEF_FUNCTION(int ,sem_add,(h2_sem_t *sem, unsigned int amt),(sem,amt));
+REDEF_FUNCTION(int ,sem_up,(h2_sem_t *sem),(sem));
+REDEF_FUNCTION(int ,sem_down,(h2_sem_t *sem),(sem));
+REDEF_FUNCTION(int ,sem_trydown,(h2_sem_t *sem),(sem));
+REDEF_FUNCTION(void ,sem_init,(h2_sem_t *sem),(sem));
+REDEF_FUNCTION(void ,sem_init_val,(h2_sem_t *sem,unsigned short val),(sem,val));
+
+/*  "blast_thread.h"  */
+
+REDEF_FUNCTION(int ,thread_create,(void *pc, void *stack, void *arg, unsigned int prio, unsigned int asid),(pc,stack,arg,prio,asid));
+REDEF_FUNCTION(void ,thread_stop,(void),());
+REDEF_FUNCTION(int ,thread_myid,(void),());
+REDEF_FUNCTION(int ,yield,(void),());
+
+/* "blast_trace.h"  */
+
+#ifndef BLAST_DEBUG
+#define BLAST_TRACE(str, ...) __VA_ARGS__
+#else
+#define BLAST_TRACE(str, ...) \
+	do { \
+		h2_printf("%s:%d: %s: >>> calling %s\n",__FILE__,__LINE__,str,#__VA_ARGS__); \
+		__VA_ARGS__; \
+		h2_printf("%s:%d: %s: <<< %s returned\n",__FILE__,__LINE__,str,#__VA_ARGS__); \
+	} while (0);
+#endif
 
 #endif

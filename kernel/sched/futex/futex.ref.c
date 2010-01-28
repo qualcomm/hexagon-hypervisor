d7e2b4f68b2f723303ad6c781e23ce1ed0d76698
Erich Plondke <erich@qti.qualcomm.com>
2010-01-28 22:59:20 +0000

    keep futex list sorted
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

--- kernel/sched/futex/futex.ref.c
+++ kernel/sched/futex/futex.ref.c
@@ -30,6 +30,7 @@
 s32_t H2K_futex_wait(u32_t *lock, u32_t val, H2K_thread_context *me)
 {
 	u32_t hashval = HASHVAL(lock);
+	H2K_thread_context *tmp;
 	BKL_LOCK();
 	if (*lock != val) {
 		/* Changed while we were trying to enqueue */
@@ -40,36 +41,38 @@ s32_t H2K_futex_wait(u32_t *lock, u32_t val, H2K_thread_context *me)
 	H2K_runlist_remove(me);
 	me->r0100 = 0;
 	me->status = H2K_STATUS_BLOCKED;
-	H2K_ring_append(&H2K_gp->futexhash[hashval],me);
+	if (H2K_gp->futexhash[hashval] == NULL) {
+		/* empty */
+		me->next = me->prev = me;
+		H2K_gp->futexhash[hashval] = me;
+	} else if (me->prio < H2K_gp->futexhash[hashval]->prio) {
+		/* me is best prio */
+		me->next = H2K_gp->futexhash[hashval];
+		me->prev = me->next->prev;
+		me->next->prev = me;
+		me->prev->next = me;
+		H2K_gp->futexhash[hashval] = me;
+	} else {
+		/* Not the best priority, search backwards through lower prios (if any) */
+		tmp = H2K_gp->futexhash[hashval]->prev;
+		while (me->prio < tmp->prio) tmp = tmp->prev;
+		me->next = tmp->next;
+		me->prev = tmp;
+		me->next->prev = me;
+		me->prev->next = me;
+	}
 	H2K_dosched(me,me->hthread);
 	return 0;
 }
 
-/* futex_find
- * Find the next thread in haystack that has a matching lock ptr 
- */
-static H2K_thread_context *futex_find(H2K_thread_context *haystack, H2K_thread_context *start, u32_t *lock)
-{
-	H2K_thread_context *tmp = start;
-	if (haystack == NULL) return haystack;
-	do {
-		if (tmp->futex_ptr == lock) return tmp;
-		tmp = tmp->next;
-	} while (tmp != haystack);
-	return NULL;
-}
-
 /* futex_resume 
- * If we are only going to wake one thread, pick the highest priority one 
- * Else, pick oldest ones from queue 
+ * Pick the first N matching elements out of the queue.
  */
 u32_t H2K_futex_resume(u32_t *lock, u32_t n_to_wake, H2K_thread_context *me)
 {
 	H2K_thread_context *tmp,*tmp2;
 	u32_t hashval = HASHVAL(lock);
 	u32_t n_woken = 0;
-	u32_t highest_prio = MAX_PRIOS, prio;
-
 	if (n_to_wake == 0) return 0;
 
 	BKL_LOCK();
@@ -78,38 +81,20 @@ u32_t H2K_futex_resume(u32_t *lock, u32_t n_to_wake, H2K_thread_context *me)
 		return 0;
 	}
 	tmp = H2K_gp->futexhash[hashval];
-	if (n_to_wake == 1) {
-		tmp2 = NULL;
-		do {
-			if (lock != tmp->futex_ptr) {
-				tmp = tmp->next;
-				continue;
-			}
-			prio = tmp->prio;
-			if (highest_prio > prio) tmp2 = tmp;
-			highest_prio = Q6_R_min_RR(highest_prio,prio);
-			tmp = tmp->next;
-		} while (H2K_gp->futexhash[hashval] != tmp);
-		if (tmp2) {
-			n_woken = 1;
-			prio = highest_prio;
-			H2K_ring_remove(&H2K_gp->futexhash[hashval],tmp2);
-			H2K_ready_append(tmp2);
-		}
-	} else {
-		tmp2 = H2K_gp->futexhash[hashval];
-		do {
-			tmp = futex_find(H2K_gp->futexhash[hashval],tmp2,lock);
-			if (tmp == NULL) break;
+	do {
+		if (tmp->futex_ptr == lock) {
 			tmp2 = tmp->next;
-			prio = tmp->prio;
-			highest_prio = Q6_R_min_RR(highest_prio,prio);
 			H2K_ring_remove(&H2K_gp->futexhash[hashval],tmp);
 			H2K_ready_append(tmp);
-			if (++n_woken >= n_to_wake) break;
-		} while (1);
-	}
-	return H2K_check_sanity_unlock(n_woken); 
+			n_woken++;
+			tmp = tmp2;
+			continue;
+		} else {
+			tmp = tmp->next;
+			if (tmp == H2K_gp->futexhash[hashval]) break;
+		}
+	} while ((n_woken < n_to_wake) && H2K_gp->futexhash[hashval]);
+	return H2K_check_sanity_unlock(n_woken);
 }
 
 void H2K_futex_init()

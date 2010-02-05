4da4f97ff48c7fd6dbac3b7c58308e98bf1a3f88
Richard Kuo <rkuo@qti.qualcomm.com>
2010-02-05 20:27:46 +0000

    changes for auto-build
    Signed-off-by: Richard Kuo <rkuo@qti.qualcomm.com>

---
+++ scripts/run_report.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+
+TAG=$1
+shift
+TYPE=$1
+shift
+
+make -f scripts/Makefile.coverage libs $*
+
+make -f scripts/Makefile.coverage all $*
+touch test.cov_fns
+make -f scripts/Makefile.coverage cov.txt $*
+make -f scripts/Makefile.coverage report.html $*
+
+mkdir -p ~rkuo/public_html/h2/$TAG 
+cp report.html ~rkuo/public_html/h2/$TAG/$TYPE.html

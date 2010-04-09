fd7df81f3c9fb1578b9e3107d80c3eba1ec8a3dd
Erich Plondke <erich@qti.qualcomm.com>
2010-04-09 03:18:19 +0000

    vm interfaces, initial work
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

---
+++ libs/h2/include/h2_vmtraps.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#ifndef H2_VMTRAPS_H
+#define H2_VMTRAPS_H 1
+
+int H2_vmtrap_clrmap(void *ptr);
+int H2_vmtrap_newptb(void *newbase);
+
+#endif
+

2a9b1833228ddb0abd064b0b882574915aac5d70
Erich Plondke <erich@qti.qualcomm.com>
2010-04-19 21:29:13 +0000

    update all this qube crap
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

--- libs/qube/include/qmemory.h
+++ libs/qube/include/qmemory.h
@@ -3,38 +3,321 @@
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
-#ifndef H2_QMEM_H
-#define H2_QMEM_H
+#ifndef _QMEMORY_H
+#define _QMEMORY_H
 
-typedef enum {
-	QMEM_CACHE_FLUSH,
-	QMEM_CACHE_INVALIDATE,
-	QMEM_CACHE_FLUSH_INVALIDATE
-} qmem_cache_op_t;
+#include <qerror.h>
+#include <qpd.h>
+#include <assert.h>
+#include <stdlib.h>
 
-typedef enum {
-        QMEM_CACHE_WRITEBACK=7,
-        QMEM_CACHE_NONE_SHARED=6,
-        QMEM_CACHE_WRITETHROUGH=5,
-        QMEM_CACHE_WRITEBACK_NONL2CACHEABLE=0,
-        QMEM_CACHE_WRITETHROUGH_NONL2CACHEABLE=1,
-        QMEM_CACHE_WRITEBACK_L2CACHEABLE=QMEM_CACHE_WRITEBACK,
-        QMEM_CACHE_WRITETHROUGH_L2CACHEABLE=QMEM_CACHE_WRITETHROUGH,
-        QMEM_CACHE_NONE
-} qmem_cache_mode_t;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-typedef enum {
-        QMEM_ICACHE,
-        QMEM_DCACHE
-} qmem_cache_type_t;
+typedef unsigned int qmem_region_t; 
+typedef unsigned int qmem_pool_t;
 
-static int qmem_cache_clean(void *data, unsigned int size, 
-	qmem_cache_op_t opcode, qmem_cache_type_t type) 
+extern qmem_pool_t qmem_default_pool;
+
+/*****************************************************************/
+/* FILE: qmemory.h                                               */
+/*                                                               */
+/* SERVICES: qmemory API                                         */
+/*                                                               */
+/* DESCRIPTION: Prototypes of qmemory API                        */
+/*****************************************************************/
+
+/************************ COPYRIGHT NOTICE ***********************/
+
+/* All data and information contained in or disclosed by this    */
+/* document is confidential and proprietary information of       */
+/* QUALCOMM, Inc and all rights therein are expressly reserved.  */
+/* By accepting this material the recipient agrees that this     */
+/* material and the information contained therein is held in     */
+/* confidence and in trust and will not be used, copied,         */
+/* reproduced in whole or in part, nor its contents revealed in  */
+/* any manner to others without the express written permission   */
+/* of QUALCOMM, Inc.                                             */
+/*****************************************************************/
+
+#define KERNEL_CACHE_OPT_THRESHHOLD (128*1024)
+
+/**
+ * Attach to a memory pool 
+ *
+ * The qmem_pool_attach is used to attach to a memory pool. 
+ *
+ * Qube memory model works with the concept of memory pool and memory region.
+ * User can specify different types of PHYSICAL memory as different memory
+ * pools.  For example EBI/SMI/TCM memory can be specified as EBI pool, SMI
+ * pool and TCM pool respectively.   
+ *
+ * Memory pools are static system information that can only be specified in xml
+ * configuration file.  All pools are by default accessible by all PDs in the
+ * system, a PD can get the handle of a pool with qmem_pool_attach function
+ * with a name.
+ *
+ * The default physical pool specified in elfweaver configuration is mapped to
+ * qmem_default_pool global handle.  Users only need to call qmem_pool_attach
+ * for non-default physical memory pool
+ *
+ * @param name        Name of the memory pool. This shall be a name specified
+ *                    in xml configuration file for a physical pool 
+ * @param pool  [OUT] memory pool object
+ * @return            EOK:       Successful attach
+*/
+static inline int qmem_pool_attach(const char *name, qmem_pool_t *pool) { *pool = 0; return EOK; }
+
+/**
+ * Create a memory region with specific attributes               
+ *
+ * The qmem_region_create is used to create a memory region. The sz
+ * specifies the size of memory region. The attr is used to specify
+ * attributes. The attributes that can be specified are physical
+ * address, memory type (defafult, tcm), mapping type (virtual,
+ * idempotent or physical contiguous), and cache properties
+ * (write-back, write-through or uncached). If attr is NULL, then
+ * default values are used
+ *
+ * Memory regions are always grouped and shared with other PDs if
+ * needed.  The grp specifies the memory group to which a memory
+ * regions belongs to.
+ *
+ * @param region  [OUT] Memory region object
+ * @param group         Memory group that the memory regions belongs to
+ * @param sz            Size of the memory region. When size is not with the
+ *                      granularity of 4K, it will be round up to 4K boundary 
+ * @param attr          Attributes of memory region
+ * @return              EOK if region is created successfuly; 
+ * @return              EMEM if out of memory; 
+ * @return              EVAL if not valid memory type/mapping type/cache type;
+ * @return              EINVALID if group is not a valid handle 
+ *
+ */
+static inline int qmem_region_create(qmem_region_t *region, size_t sz, qmem_pool_t pool,
+                       const qmem_region_attr_t *attr)
 {
-	return 0;
+	if ((*region = (unsigned int)malloc(sz)) != 0) return EOK;
+	return EMEM;
 }
 
-typedef unsigned int qmem_region_h;
-typedef unsigned int qmem_pool_t;
+/**
+ * Remove a memory region
+ *
+ * @param region  Memory region object
+ * @return        EOK if deletion is successful;
+ * @return        EINVALID if region is not a valid handle; 
+ */
+//static inline int qmem_region_delete(qmem_region_t region) { return EOK; } 
+static inline int qmem_region_delete(qmem_region_t region) { free((void *)region); return EOK; }
+
+/**
+ * Perform a cache operation (flush or invaldate or both)
+ *
+ * Limitaion: Currently, Flush & Invalidate operations are
+ * "for" loop of user flush instructions.  Further
+ * investigation is needed to decide under which
+ * circumstances privileged flush instruction should be used,
+ * because possible system call or IPC call may have other
+ * costs 
+ * 
+ * @param data    Pointer to data or instruction memory that needs to be flushed
+ * @param size    Size of the data
+ * @param opcode  Operation type (flush or invalidate or both)
+ * @param type    Cache type (instruction or data)
+ * @return        EOK if operation is successful 
+ * @return        EVAL if type is not valid; 
+ */
+void qmem_blast_inv_icache(const void *data, size_t size);
+void qmem_blast_clean_dcache(const void *data, size_t size);
+void qmem_blast_inv_dcache(const void *data, size_t size);
+void qmem_blast_cleaninv_dcache(const void *data, size_t size);
+int qmem_kernel_cache_opt(const void * data, size_t size, const unsigned int type);
+
+enum cache_opt {
+    KERNEL_DCCLEAN=0,
+    KERNEL_DCINV,
+    KERNEL_DCCLEANINV,
+    KERNEL_ICINV
+}; 
+
+//
+//For detailed arch information, refer to Chapter 6(Cache Memory) in QDSP6 V2
+//Architecture System-Level Specification
+//
+
+static inline int qmem_cache_clean(const void *data, size_t size, const qmem_cache_op_t opcode, const qmem_cache_type_t type)
+{
+	if (type == QMEM_ICACHE) {
+		switch (opcode) {
+			case QMEM_CACHE_FLUSH: return EOK;
+			case QMEM_CACHE_INVALIDATE: qmem_blast_inv_icache(data,size); return EOK;
+			case QMEM_CACHE_FLUSH_INVALIDATE: qmem_blast_inv_icache(data,size); return EOK;
+			default: return EVAL;
+		}
+	} else if (type == QMEM_DCACHE) {
+		switch (opcode) {
+			case QMEM_CACHE_FLUSH: 
+				qmem_blast_clean_dcache(data,size); return EOK;
+			case QMEM_CACHE_INVALIDATE: 
+				qmem_blast_inv_dcache(data,size); return EOK;
+			case QMEM_CACHE_FLUSH_INVALIDATE: 
+				qmem_blast_cleaninv_dcache(data,size); return EOK;
+			default: return EVAL;
+		}
+	}
+	return EVAL;
+}
+
+static inline int qmem_cache_clean_aligning(const void *data, size_t size, const qmem_cache_op_t opcode, const qmem_cache_type_t type)
+{
+	//Don't fail, rather do what L4 did, which is align the memory/size and proceed.
+	char * byte_addr = (char*)data;
+	/* Align down to 2 ^ 5 = 32 */
+	char * aligned_addr = (char*)(((unsigned int)byte_addr) & (0xffffffe0));
+	size += (((unsigned int)byte_addr) % 32);
+	if ((size%32)!=0) size+=(32-(size%32));
+	return qmem_cache_clean(aligned_addr, size, opcode, type);
+}
+
+/**
+ * Initialize attribute object with default values. The default value for
+ * physical address and virtual address is INVALID_ADDR (-1),
+ * QMEM_MEMORY_DEFAULT for memory type, QMEM_MAPPING_VIRTUAL for mapping type,
+ * and QMEM_CACHE_WRITEBACK_SHARED for cacheing property
+ *
+ * @param attr  Attributes object
+ * @return      void 
+ */
+static inline void qmem_region_attr_init(qmem_region_attr_t *attr){
+	attr->mapping_type = QMEM_MAPPING_VIRTUAL;
+	attr->cache_mode = QMEM_CACHE_WRITEBACK;
+	attr->physaddr =(bl_addr_t) -1;
+	attr->virtaddr =(bl_addr_t) -1;
+	attr->type = QMEM_REGION_LOCAL;
+}
+
+/**
+* Get attributes of a memory region
+*
+* @param region        Memory region object
+* @param attr   [OUT]  Memory region attributes
+* @return              EOK if successful, error code otherwise
+*/
+static inline int qmem_region_get_attr(qmem_region_t region, qmem_region_attr_t *attr)
+{
+	attr->physaddr = region;
+	attr->virtaddr = region;
+}
+
+/**
+ * Set memory type. The possible values are QMEM_MEMORY_DEFAULT and
+ * QMEM_MEMORY_TCM
+ *
+ * @param attr  Attributes object
+ * @param type  Memory type
+ * @return      void 
+ */
+static inline void qmem_region_attr_settype(qmem_region_attr_t *attr, qmem_region_type_t type){
+    attr->type = type;
+}
+
+/**
+ * Get memory type
+ *
+ * @param attr  Attributes object
+ * @param type  Memory type
+ * @return      void 
+ */
+static inline void qmem_region_attr_gettype(qmem_region_attr_t *attr, qmem_region_type_t *type){
+    (*type) = attr->type;
+}
+
+/**
+ * Set physical address attribute
+ *
+ * @param attr  Attributes object
+ * @param addr  Physical address where memory region should be created
+ * @return      void
+ */
+static inline void qmem_region_attr_setphysaddr(qmem_region_attr_t *attr, bl_addr_t addr){
+    attr->physaddr = addr;
+}
+
+/**
+ * Get physical address attribute
+ *
+ * @param attr  Attributes object
+ * @param addr  Physical address where memory region should be created
+ * @return      void 
+ */
+static inline void qmem_region_attr_getphysaddr(qmem_region_attr_t *attr, unsigned int *addr){
+    (*addr) = attr->physaddr;
+}
+
+/**
+ * Get virtual address attribute
+ *
+ * We don't have a setvirtaddr helper api because users can not specify virtual
+ * address for a memory region
+ *
+ * @param attr  Attributes object
+ * @param addr  Virtual address where memory region should be created
+ * @return      void 
+ */
+static inline void qmem_region_attr_getvirtaddr(qmem_region_attr_t *attr, unsigned int *addr){
+    (*addr) = attr->virtaddr;
+}
+
+/**
+ * Set memory mapping type. The possible values are QMEM_MAPPING_VIRTUAL,
+ * QMEM_MAPPING_IDEMPOTENT and QMEM_MAPPING_PHYS_CONTIGUOUS
+ *
+ * @param attr     Attributes object
+ * @param mapping  Mapping property
+ * @return         void 
+ */
+static inline void qmem_region_attr_setmapping(qmem_region_attr_t *attr, qmem_mapping_t mapping){
+    attr->mapping_type = mapping;
+}
+
+/**
+ * Get memory mapping type
+ *
+ * @param attr     Attributes object
+ * @param mapping  Mapping property
+ * @return         void
+ */
+static inline void qmem_region_attr_getmapping(qmem_region_attr_t *attr, qmem_mapping_t *mapping){
+    (*mapping) = attr->mapping_type;
+}
+
+/**
+ * Set cache property. The possible values are QMEM_CACHE_WRITEBACK,
+ * QMEM_CACHE_WRITETHROUGH and QMEM_CACHE_NONE
+ *
+ * @param attr  Attributes object
+ * @param mode  Cacheability property
+ * @return      void
+ */
+static inline void qmem_region_attr_setcachemode(qmem_region_attr_t *attr, qmem_cache_mode_t mode){
+    attr->cache_mode = mode;
+}
+
+/**
+ * Get cache property
+ *
+ * @param attr  Attributes object
+ * @param mode  Cacheability property
+ * @return      void
+ */
+static inline void qmem_region_attr_getcachemode(qmem_region_attr_t *attr, qmem_cache_mode_t *mode){
+    (*mode) = attr->cache_mode;
+}
+
+#ifdef __cplusplus
+} /* closing brace for extern "C" */
+#endif
 
 #endif
--- libs/qube/include/qtypes.h
+++ libs/qube/include/qtypes.h
@@ -9,6 +9,10 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define QTHREAD_NAME_LEN       16 
 #define QTHREAD_MIN_STACKSIZE  4096
 #define QTHREAD_MAX_STACKSIZE  1048576
@@ -18,6 +22,9 @@
 
 typedef uint32_t  bl_addr_t;
 typedef uint32_t  qobject_t;
+
+typedef unsigned int qmem_region_t; 
+typedef unsigned int qmem_pool_t;
 
 /**
  * Mutex type
@@ -46,4 +53,102 @@ typedef enum {
         QPERM_FULL=QPERM_READ|QPERM_WRITE|QPERM_EXECUTE,
 } qperm_t;
 
+/**
+ * Mapping type
+ * 
+ * QMEM_MAPPING_VIRTUAL is the default mode, in which system will
+ * pick up an availabe range of virtual address, and mapped to
+ * contiguous or seperated physical address;    
+ *
+ * In QMEM_MAPPING_IDEMPOTENT mode, the virtual address will be same
+ * as the phyiscal address. 
+ *
+ * In QMEM_MAPPING_PHYS_CONTIGUOUS mode, the virtual address might
+ * not be  same as the phyiscal address. But physical address of the
+ * memory region is guaranted to be contiguous. 
+ */
+typedef enum {
+        QMEM_MAPPING_VIRTUAL=0,
+        QMEM_MAPPING_PHYS_CONTIGUOUS,
+} qmem_mapping_t;
+
+typedef enum {
+        QMEM_MAPPING_VIRTUAL=0,
+        QMEM_MAPPING_PHYS_CONTIGUOUS,
+} qmem_mapping_t;
+
+/**
+ * Cache mode type
+ */
+typedef enum {
+#if __QDSP6_ARCH__ >= 2
+        QMEM_CACHE_WRITEBACK=7,
+        QMEM_CACHE_NONE_SHARED=6,
+        QMEM_CACHE_WRITETHROUGH=5,
+        QMEM_CACHE_WRITEBACK_NONL2CACHEABLE=0,
+        QMEM_CACHE_WRITETHROUGH_NONL2CACHEABLE=1,
+        QMEM_CACHE_WRITEBACK_L2CACHEABLE=QMEM_CACHE_WRITEBACK,
+        QMEM_CACHE_WRITETHROUGH_L2CACHEABLE=QMEM_CACHE_WRITETHROUGH,
+        QMEM_CACHE_NONE
+#else
+        QMEM_CACHE_WRITEBACK=0,
+        QMEM_CACHE_WRITETHROUGH=1,
+        QMEM_CACHE_NONE
+        
 #endif
+} qmem_cache_mode_t;
+
+/**
+ * Cache type 
+ *
+ * Specifying data cache or instruction cache
+ */
+typedef enum {
+        QMEM_ICACHE,
+        QMEM_DCACHE
+} qmem_cache_type_t;
+
+/**
+ * Cache operation type
+ */
+typedef enum { 
+    QMEM_CACHE_FLUSH,
+    QMEM_CACHE_INVALIDATE,
+    QMEM_CACHE_FLUSH_INVALIDATE
+} qmem_cache_op_t;
+
+/**
+ * Memory access permision
+ */
+typedef enum {
+        QMEM_PERM_READ=0x4,
+        QMEM_PERM_WRITE=0x2,
+        QMEM_PERM_EXECUTE=0x1,
+} qmem_perm_t;
+
+typedef enum {
+	QMEM_REGION_LOCAL = 0,
+	QMEM_REGION_SHARED
+} qmem_region_type_t;
+
+/**
+ * Memory attribute
+ *
+ * Users can specify physaddr in QMEM_MAPPING_IDEMPOTENT mode and
+ * QMEM_MAPPING_PHYS_CONTIGUOUS mode.  Users can not specify virtaddr for a
+ * memory region, it can only be queried by qmem_attr_getvirtaddr function 
+ */
+typedef struct {
+    qmem_mapping_t    mapping_type;
+    qmem_cache_mode_t cache_mode;
+    bl_addr_t            physaddr;
+    bl_addr_t            virtaddr;
+    qmem_region_type_t   type;
+} qmem_region_attr_t;
+
+#ifdef __cplusplus
+} /* closing brace for extern "C" */
+#endif
+
+#endif
+

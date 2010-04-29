e4f515da3775651e80219673db5cb25854d44c6b
Erich Plondke <erich@qti.qualcomm.com>
2010-04-29 19:56:26 +0000

    error handling header
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

---
+++ libs/h2/include/h2_error.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#ifndef H2_ERROR_H
+#define H2_ERROR_H 1
+
+void h2_handle_errors();
+void h2_set_handler(int eventnum, void (*fn)());
+
+#endif
+

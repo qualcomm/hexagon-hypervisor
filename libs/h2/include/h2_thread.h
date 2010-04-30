c01ae596c4ba47ca99ecc381aba76fbb35895234
Erich Plondke <erich@qti.qualcomm.com>
2010-04-30 14:10:38 +0000

    fix thread create signature
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

--- libs/h2/include/h2_thread.h
+++ libs/h2/include/h2_thread.h
@@ -12,7 +12,7 @@
 #ifndef H2_THREAD_H
 #define H2_THREAD_H 1
 
-int h2_thread_create(void *pc, void *stack, void *arg, unsigned int prio, unsigned int asid);
+int h2_thread_create(void *pc, void *stack, void *arg, unsigned int prio, unsigned int trapmask);
 void h2_thread_stop(void);
 int h2_thread_myid(void);
 void h2_yield(void);

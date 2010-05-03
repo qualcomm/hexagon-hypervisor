3ffd1e69451e0da110e483441144c1302266c9c7
Richard Kuo <rkuo@qti.qualcomm.com>
2010-05-03 20:53:51 +0000

    turned this into more of a bona fide testcase
    Signed-off-by: Richard Kuo <rkuo@qti.qualcomm.com>

--- libs/h2/test/h2_fastint/main.c
+++ libs/h2/test/h2_fastint/main.c
@@ -5,43 +5,108 @@
 
 #include <h2.h>
 
-#define TEST_INT 0
-#define TEST_SIGMASK 0x1
+#define info(...) { h2_printf("INFO:  "); h2_printf(__VA_ARGS__);}
+#define warn(...) { h2_printf("WARNING:  "); h2_printf(__VA_ARGS__);}
+#define debug(...) { h2_printf("DEBUG:  "); h2_printf(__VA_ARGS__);}
+#define error(...) { h2_printf("ERROR:  "); h2_printf(__VA_ARGS__); FAIL("");}
 
-char *context_space[256 * 64];
+void FAIL(const char *str)
+{
+	exit(1);
+}
 
-h2_anysignal_t int_sig;
-h2_mutex_t mutex;
-h2_sem_t   sem;
+#define TEST_INT	31
+#define TEST_SIGMASK	0x1
+#define TEST_THREADS	3
+
+#define SIGNAL_CNT	20
+#define WDOG_TIMEOUT	100
+
+//  Should have a watchdog (main) thread, a timer thread, and a signal watcher thread
+
+unsigned long context_space[1024];
+unsigned long stack_space[TEST_THREADS][1024]; 
+
+volatile unsigned long count = 0;
+
+h2_anysignal_t	int_sig;
+h2_mutex_t	mutex;
+h2_sem_t	sem;
 
 void int2sig(int intnum) 
 {
-	//h2_anysignal_set(&int_sig,TEST_SIGMASK);  // doesn't work
-	//h2_mutex_unlock(&mutex);  //  works
-	h2_sem_add(&sem,1);  //  works
+	h2_anysignal_set(&int_sig,TEST_SIGMASK);  
+	//h2_mutex_unlock(&mutex);
+	//h2_sem_add(&sem,1);
+}
+
+void timer(int dummy)
+{
+	int i,j;
+	info("Timer started\n");
+
+	for (i=0; i<SIGNAL_CNT; i++) {
+		for (j=0; j<1000; j++) {
+			asm volatile("nop;");
+		}
+		info("Sending interrupt\n");
+		asm volatile("R0 = #1; swi(R0);":::"r0");
+	}
+
+	h2_thread_stop();  //  weird things happen without this here.
+}
+
+void watcher(int dummy)
+{
+	info("Watcher sterted\n");
+
+	while (1) {
+		h2_anysignal_wait(&int_sig, TEST_SIGMASK);
+		//h2_sem_down(&sem);
+		//h2_mutex_lock(&mutex);
+		count++;
+		info("Got signal\n");
+		h2_anysignal_clear(&int_sig, TEST_SIGMASK);
+	}
 }
 
 int main()
 {
+	int i,j;
 
 	h2_init(0);
 	h2_config_add_thread_storage(context_space,sizeof(context_space));
-	h2_printf("Starting\n");
+	info("Starting\n");
 
 	h2_anysignal_init(&int_sig);
+	h2_anysignal_clear(&int_sig, TEST_SIGMASK);
+
 	h2_mutex_init(&mutex);
 	h2_mutex_lock(&mutex);
+
 	h2_sem_init(&sem);
+	h2_sem_down(&sem);
 
 	h2_register_fastint(TEST_INT,int2sig);
+	h2_thread_create(watcher,&stack_space[1],0,0,0xffffffff);  // stackgrowsup
+	h2_thread_create(timer,&stack_space[2],0,0,0xffffffff);
 
-	while (1) {
-		//h2_anysignal_wait(&int_sig, TEST_SIGMASK);
-		h2_sem_down(&sem);
-		//h2_mutex_lock(&mutex);
-		h2_printf("Got Interrupt!\n");
-		//h2_anysignal_clear(&int_sig, TEST_SIGMASK);
+	for (i=0; i<WDOG_TIMEOUT; i++) {
+		for (j=0; j<1000; j++) {
+			asm volatile("nop;");
+		}
 	}
+
+	info("Checking results\n");
+
+	if (count != SIGNAL_CNT) {
+		error("Wrong signal count\n");
+	}
+	else {
+		info("TEST PASSED\n");
+	}
+
+	return 0;
 
 }
 

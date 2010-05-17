81755a90e5516933a4f1d2c06eb5d0bd386684c9
Richard Kuo <rkuo@qti.qualcomm.com>
2010-05-17 17:55:28 +0000

    unfinished test
    Signed-off-by: Richard Kuo <rkuo@qti.qualcomm.com>

---
+++ libs/h2/test/h2_map/main.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#include <h2.h>
+
+#define info(...) { h2_printf("INFO:  "); h2_printf(__VA_ARGS__);}
+#define warn(...) { h2_printf("WARNING:  "); h2_printf(__VA_ARGS__);}
+#define debug(...) { h2_printf("DEBUG:  "); h2_printf(__VA_ARGS__);}
+#define error(...) { h2_printf("ERROR:  "); h2_printf(__VA_ARGS__); FAIL("");}
+
+void FAIL(const char *str)
+{
+        h2_printf(str);
+        exit(1);
+}
+
+int main() 
+{
+	error("Test not done yet\n");
+	return(0);
+}

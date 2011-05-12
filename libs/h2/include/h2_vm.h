297a89a87b5707781410f62eadb0df56ecc4b5f3
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2011-05-12 13:42:01 -0500

    add lib wrapper for vmboot trap
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

---
+++ libs/h2/include/h2_vm.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+/*
+ * h2_vm.h
+ * 
+ * Boot a VM
+ */
+
+#ifndef H2_VM_H
+#define H2_VM_H 1
+
+/* basically thread_create but with a vmblock */
+int h2_vmboot(void *pc, void *stack, unsigned int arg, unsigned int prio, void *vmblock);
+
+#endif
+

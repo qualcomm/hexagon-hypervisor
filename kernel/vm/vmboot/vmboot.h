7e469075d1ece370b32b684d4f14ec2b9918dc10
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2011-05-12 14:35:25 -0500

    Add vmboot
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

---
+++ kernel/vm/vmboot/vmboot.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#ifndef H2K_VMBOOT_H
+#define H2K_VMBOOT_H 1
+
+#include <vm.h>
+#include <context.h>
+
+s32_t H2K_vmboot(u32_t pc, u32_t sp, u32_t arg1, u32_t prio, H2K_vmblock_t *vmblock, H2K_thread_context *me) IN_SECTION(".text.misc.vmboot");
+
---
+++ kernel/vm/vmboot/vmboot.ref.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#include <vmboot.h>
+
+s32_t H2K_vmboot(u32_t pc, u32_t sp, u32_t arg1, u32_t prio, H2K_vmblock_t *vmblock, H2K_thread_context *me)
+{
+	

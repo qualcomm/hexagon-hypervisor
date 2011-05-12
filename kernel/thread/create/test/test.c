9a206655f846251046714260bd9d5845f543d0b3
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2011-05-12 14:15:13 -0500

    Replace trapmask arguments with 0 (null value for vmblock). Check that vmblock pointer in new thread is null.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- kernel/thread/create/test/test.c
+++ kernel/thread/create/test/test.c
@@ -16,7 +16,7 @@
 #include <checker_kernel_locked.h>
 #include <globals.h>
 
-u32_t H2K_thread_create(u32_t pc, u32_t sp, u32_t arg1, u32_t prio, u32_t trapmask, H2K_thread_context *me);
+u32_t H2K_thread_create(u32_t pc, u32_t sp, u32_t arg1, u32_t prio, H2K_vmblock_t *vmblock, H2K_thread_context *me);
 void H2K_interrupt_restore();
 
 void FAIL(const char *str)
@@ -65,40 +65,42 @@ int main()
 	b.ugpgp = c.ugpgp = 0x0;
 	b.status = c.status = H2K_STATUS_DEAD;
 	a.trapmask = 0x55ffffff;
+	a.vmblock = 0x1337beef;
 
-	if (H2K_thread_create((u32_t)test_thread,((u32_t)(&stack)),0xdeadbeef,2,0xffffffff,&a)
+	if (H2K_thread_create((u32_t)test_thread,((u32_t)(&stack)),0xdeadbeef,2,0x0,&a)
 		!= 0xffffffff) FAIL("Created thread w/o storage");
 	H2K_gp->free_threads = &b;
 	b.next = &c;
 	c.next = NULL;
-	if (H2K_thread_create(((u32_t)test_thread)+1,((u32_t)(&stack)),0xdeadbeef,2,0xffffffff,&a) 
+	if (H2K_thread_create(((u32_t)test_thread)+1,((u32_t)(&stack)),0xdeadbeef,2,0x0,&a) 
 		!= 0xffffffff) FAIL("Created thread w/ misaligned pc");
-	if (H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack))+1,0xdeadbeef,2,0xffffffff,&a) 
+	if (H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack))+1,0xdeadbeef,2,0x0,&a) 
 		!= 0xffffffff) FAIL("Created thread w/ misaligned sp");
-	if (H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,902,0xffffffff,&a) 
+	if (H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,902,0x0,&a) 
 		!= 0xffffffff) FAIL("Created thread w/ misaligned sp");
 
 	if (TH_saw_check_sanity != 0) FAIL("Called check_sanity on failure");
 
-	if (H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,2,0xff33ffff,&a) 
+	if (H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,2,0x0,&a) 
 		!= (u32_t)(&b)) FAIL("Failed to create expected thread");
 	if (TH_saw_check_sanity == 0) FAIL("Did not call check_sanity");
 	if (H2K_gp->ready[2] != &b) FAIL("Thread inserted incorrectly into ready list");
 	if (b.prio != 2) FAIL("thread priority set wrong");
 	if (b.status == H2K_STATUS_DEAD) FAIL("status field incorrect");
 	if (b.r0100 != 0x00000000deadbeefULL) FAIL("Incorrect argument");
-	if (b.trapmask != 0x5533ffff) FAIL("Incorrect trap mask");
+	if (b.trapmask != 0x55ffffff) FAIL("Incorrect trap mask");
 	if (b.continuation != H2K_interrupt_restore) FAIL("Incorrect continuation");
 	if ((b.ssrelr & 0x00000000FFFFFFFF) != ((u32_t)test_thread)) FAIL("Incorrect return address");
 	if ((b.ssrelr >> 32) != (a.ssrelr >> 32)) FAIL("Incorrect inheritance of SSR");
 	if (b.ugpgp != a.ugpgp) FAIL("Incorrect inheritance of UGP/GP");
+	if (b.vmblock != 0) FAIL("vmblock is non-NULL");
 
 	TH_saw_check_sanity = 0;
-	if (H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,2,0xffffffff,&a) 
+	if (H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,2,0x0,&a) 
 		!= (u32_t)(&c)) FAIL("Failed to create expected thread");
 	if (TH_saw_check_sanity == 0) FAIL("Did not call check_sanity");
 
-	if (H2K_thread_create((u32_t)test_thread,((u32_t)(&stack)),0xdeadbeef,2,0xffffffff,&a) 
+	if (H2K_thread_create((u32_t)test_thread,((u32_t)(&stack)),0xdeadbeef,2,0x0,&a) 
 		!= 0xffffffff) FAIL("Created thread w/o storage");
 
 	puts("TEST PASSED\n");

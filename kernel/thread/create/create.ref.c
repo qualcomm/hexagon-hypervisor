0dfd4aa94bbb0f03c705193a902ec2b596760604
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2011-05-12 14:10:50 -0500

    Replace trapmask parameter with vmblock pointer. If vmblock pointer valid, take trapmask and priority limit from vmblock, else inherit.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- kernel/thread/create/create.ref.c
+++ kernel/thread/create/create.ref.c
@@ -20,16 +20,30 @@
 
 void H2K_interrupt_restore();
 
-IN_SECTION(".text.misc.create") s32_t H2K_thread_create(u32_t pc, u32_t sp, u32_t arg1, u32_t prio, u32_t trapmask, H2K_thread_context *me)
+IN_SECTION(".text.misc.create") s32_t H2K_thread_create(u32_t pc, u32_t sp, u32_t arg1, u32_t prio, H2K_vmblock_t *vmblock, H2K_thread_context *me)
 {       
 	H2K_thread_context *tmp;
-	u32_t myssr;
+	u32_t bestprio;
+	u32_t trapmask;
+
+	u32_t myssr; 									/* FIXME: is this needed? */
 	myssr = me->ssr;
+
+	/* get priority and trapmask from VM block if valid */
+	if (vmblock) {
+		bestprio = vmblock->bestprio;
+		trapmask = vmblock->trapmask;
+	}
+	else {
+		bestprio = me->base_prio;
+		trapmask = me->trapmask;
+	}
+
 	if (prio > MAX_PRIO) return -1;        // bad prio
-	if ((sp & 7) != 0) return -1;           // bad stack pointer alignment
-	if ((pc & 3) != 0) return -1;           // bad pc alignment
-//	Leave this check off for now
-//	if (prio < me->base_prio) return -1;	// can't spawn higher-priority children
+	if ((sp & 7) != 0) return -1;          // bad stack pointer alignment
+	if ((pc & 3) != 0) return -1;          // bad pc alignment
+	if (prio < bestprio) return -1;	       // priority better than allowed
+
 	BKL_LOCK(&H2K_bkl);
 	if (H2K_gp->free_threads == NULL) {
 		BKL_UNLOCK(&H2K_bkl);
@@ -46,9 +60,17 @@ IN_SECTION(".text.misc.create") s32_t H2K_thread_create(u32_t pc, u32_t sp, u32_
 	tmp->r0100 = arg1;
 	H2K_asid_table_inc(H2K_mem_asid_table[me->ssr_asid].ptb);
 	tmp->ccr = me->ccr;
-	tmp->trapmask = trapmask & me->trapmask;
+	tmp->trapmask = trapmask;
 	tmp->continuation = H2K_interrupt_restore;
-        H2K_ready_append(tmp);
+
+	if (vmblock) {
+		if (vmblock->num_cpus == vmblock->max_cpus) return -1; // no more vcpus
+		tmp->vmcpu = vmblock->num_cpus++;
+
+		tmp->vmstatus = 0;            // all clear
+		tmp->vmblock = vmblock;
+	}
+	H2K_ready_append(tmp);
 	return H2K_check_sanity_unlock((u32_t)tmp);
 }
 

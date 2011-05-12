4bdc977918b26678a268a7a2d6eb8f011f05c6ff
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2011-05-12 14:25:17 -0500

    Remove trapmask arg.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- libs/h2/include/h2_thread.h
+++ libs/h2/include/h2_thread.h
@@ -12,7 +12,8 @@
 #ifndef H2_THREAD_H
 #define H2_THREAD_H 1
 
-int h2_thread_create(void *pc, void *stack, void *arg, unsigned int prio, unsigned int trapmask);
+/* trap0 handler forces vmblock to NULL, so no need to declare it here */
+int h2_thread_create(void *pc, void *stack, void *arg, unsigned int prio);
 void h2_thread_stop(void);
 int h2_thread_myid(void);
 void h2_yield(void);

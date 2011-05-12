24257f2503aa9c15fe19bc21532c736fbf9b62e5
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2011-05-12 14:24:15 -0500

    Add config_vmblock_*.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- libs/h2/include/h2_config.h
+++ libs/h2/include/h2_config.h
@@ -6,7 +6,18 @@
 #ifndef H2_CONFIG_H
 #define H2_CONFIG_H 1
 
+typedef enum {
+	SET_STORAGE_IDENT_PMAP,
+	SET_PRIO_TRAPMASK,
+	SET_CPUS_INTS,
+	MAP_PHYS_INTR,
+	NUM_OPS
+} vmblock_init_op_t;
+
 void h2_config_add_thread_storage(void *buf, unsigned int size);
+
+unsigned int h2_config_vmblock_size(char num_cpus, short num_ints);
+void *h2_config_vmblock_init(void *ptr, vmblock_init_op_t op, unsigned int arg1, unsigned int arg2);
 
 #endif
 

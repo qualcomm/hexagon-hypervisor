f2f7c4b7810bc303feb3ae97655bc43613717134
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2011-05-16 17:05:07 -0500

    Pass translation_type in h2_vmtrap_newptb().
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- libs/h2/include/h2_vmtraps.h
+++ libs/h2/include/h2_vmtraps.h
@@ -6,9 +6,15 @@
 #ifndef H2_VMTRAPS_H
 #define H2_VMTRAPS_H 1
 
+typedef enum {
+	H2K_ASID_TRANS_TYPE_LINEAR,
+	H2K_ASID_TRANS_TYPE_TABLE,
+	H2K_ASID_TRANS_TYPE_XXX_LAST
+} translation_type;
+
 int h2_vmtrap_setvec(void *ptr);
 int h2_vmtrap_clrmap(void *ptr);
-int h2_vmtrap_newptb(void *newbase);
+int h2_vmtrap_newptb(void *newbase, translation_type type);
 void h2_vmtrap_setregs(unsigned int g0, unsigned int g1, unsigned int g2, unsigned int g3);
 void h2_vmtrap_getregs(unsigned int *regsptr);
 

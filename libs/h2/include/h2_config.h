9f3a232021fad3652c245fdca4e1b26c18a55829
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2011-05-16 17:03:04 -0500

    Fix enum, args.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- libs/h2/include/h2_config.h
+++ libs/h2/include/h2_config.h
@@ -7,16 +7,20 @@
 #define H2_CONFIG_H 1
 
 typedef enum {
-	SET_STORAGE_IDENT_PMAP,
+	SET_STORAGE_IDENT,
+	SET_PMAP_TYPE,
 	SET_PRIO_TRAPMASK,
 	SET_CPUS_INTS,
 	MAP_PHYS_INTR,
 	NUM_OPS
 } vmblock_init_op_t;
 
-void h2_config_add_thread_storage(void *buf, unsigned int size);
+unsigned int h2_config_add_thread_storage(void *buf, unsigned int size);
+
+unsigned int h2_config_setfatal(void *handler);
 
 unsigned int h2_config_vmblock_size(char num_cpus, short num_ints);
+
 void *h2_config_vmblock_init(void *ptr, vmblock_init_op_t op, unsigned int arg1, unsigned int arg2);
 
 #endif

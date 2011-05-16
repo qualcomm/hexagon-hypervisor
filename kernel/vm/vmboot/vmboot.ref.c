493ba2c331546653632224c90857f272ef014cc6
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2011-05-16 16:57:13 -0500

    Wrap H2K_thread_create().
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- kernel/vm/vmboot/vmboot.ref.c
+++ kernel/vm/vmboot/vmboot.ref.c
@@ -3,8 +3,10 @@
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
-#include <vmboot.h>
+#include <create.h>
 
 s32_t H2K_vmboot(u32_t pc, u32_t sp, u32_t arg1, u32_t prio, H2K_vmblock_t *vmblock, H2K_thread_context *me)
 {
 	
+	return H2K_thread_create(pc, sp, arg1, prio, vmblock, me);
+}

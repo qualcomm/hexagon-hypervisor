b3ac3782a3d09afecc1538c912d80b1f95d9c41d
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2011-05-26 13:12:04 -0500

    Test code path for vmblock != NULL.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- kernel/thread/create/test/test.c
+++ kernel/thread/create/test/test.c
@@ -16,6 +16,7 @@
 #include <checker_kernel_locked.h>
 #include <globals.h>
 #include <vm.h>
+#include <asid.h>
 
 u32_t H2K_thread_create(u32_t pc, u32_t sp, u32_t arg1, u32_t prio, H2K_vmblock_t *vmblock, H2K_thread_context *me);
 void H2K_interrupt_restore();
@@ -27,7 +28,7 @@ void FAIL(const char *str)
 	exit(1);
 }
 
-static H2K_thread_context a,b,c;
+static H2K_thread_context a,b,c,d;
 
 u32_t TH_saw_check_sanity = 0;
 H2K_thread_context *TH_me = NULL;
@@ -53,6 +54,12 @@ void test_thread(unsigned int arg)
 
 unsigned long long int stack;
 
+H2K_vmblock_t vm;
+H2K_thread_context *vmcontext[2];
+
+s32_t asid;
+s32_t ret;
+
 int main() 
 {
 	__asm__ __volatile(" r16 = %0 " : : "r"(&H2K_kg));
@@ -60,25 +67,30 @@ int main()
 	H2K_readylist_init();
 	H2K_lowprio_init();
 	H2K_thread_init();
+	H2K_asid_table_init();
 
-	a.prio = b.prio = c.prio = 0;
+	asid = H2K_asid_table_inc(0xfeedf00f, H2K_ASID_TRANS_TYPE_LINEAR);
+
+	a.prio = b.prio = c.prio = d.prio = 0;
 	a.ugpgp = 0x12345;
-	b.ugpgp = c.ugpgp = 0x0;
-	b.status = c.status = H2K_STATUS_DEAD;
+	b.ugpgp = c.ugpgp = d.ugpgp = 0x0;
+	b.status = c.status = d.status = H2K_STATUS_DEAD;
 	a.trapmask = 0x55ffffff;
 	a.vmblock = (H2K_vmblock_t *)0x1337beef;
+	a.ssr_asid = asid;
 
 	if (H2K_thread_create((u32_t)test_thread,((u32_t)(&stack)),0xdeadbeef,2,0x0,&a)
 		!= 0xffffffff) FAIL("Created thread w/o storage");
 	H2K_gp->free_threads = &b;
 	b.next = &c;
-	c.next = NULL;
+	c.next = &d;
+	d.next = NULL;
 	if (H2K_thread_create(((u32_t)test_thread)+1,((u32_t)(&stack)),0xdeadbeef,2,0x0,&a) 
 		!= 0xffffffff) FAIL("Created thread w/ misaligned pc");
 	if (H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack))+1,0xdeadbeef,2,0x0,&a) 
 		!= 0xffffffff) FAIL("Created thread w/ misaligned sp");
 	if (H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,902,0x0,&a) 
-		!= 0xffffffff) FAIL("Created thread w/ misaligned sp");
+		!= 0xffffffff) FAIL("Created thread w/ bad prio");
 
 	if (TH_saw_check_sanity != 0) FAIL("Called check_sanity on failure");
 
@@ -96,9 +108,39 @@ int main()
 	if (b.ugpgp != a.ugpgp) FAIL("Incorrect inheritance of UGP/GP");
 	if (b.vmblock != 0) FAIL("vmblock is non-NULL");
 
+	vm.max_cpus = 2;
+	vm.num_cpus = 2; // initially full
+	vm.bestprio = 5;
+	vm.trapmask = 0xfa1a1a1a;
+	vm.cpu_contexts = vmcontext;
+	vm.pmap = 0xb1ab1ab1;
+	vm.pmap_type = H2K_ASID_TRANS_TYPE_TABLE;
+
+	/* so we can check if properly decremented */
+	asid = H2K_asid_table_inc(vm.pmap, H2K_ASID_TRANS_TYPE_TABLE);
+
+	ret = H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,6,&vm,&a);
+	/* asid count should have gone to 2 and then back to 1 */
+	if (H2K_mem_asid_table[asid].count != 1) FAIL("Bad asid count");
+	if (ret != -1) FAIL("Exceeded max_cpus");
+	vm.num_cpus = 1;
+
+	ret = H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,4,&vm,&a);
+	if (ret != -1) FAIL("Exceeded vm bestprio");
+
+	ret = H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,6,&vm,&a);
+	if (ret == -1) FAIL("Unexpected error");
+
+	if (c.trapmask != 0xfa1a1a1a) FAIL("Bad vm trapmask");
+	if (c.ssr_asid != asid) FAIL("Bad vm asid");
+	if (vm.num_cpus != 2) FAIL("Bad vm num_cpus");
+	if (c.vmcpu != 1) FAIL("Bad vmcpu");
+	if (vmcontext[1] != &c) FAIL("Bad vm context");
+	if (c.vmblock != &vm) FAIL("Bad vmblock");
+
 	TH_saw_check_sanity = 0;
 	if (H2K_thread_create(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,2,0x0,&a) 
-		!= (u32_t)(&c)) FAIL("Failed to create expected thread");
+		!= (u32_t)(&d)) FAIL("Failed to create expected thread");
 	if (TH_saw_check_sanity == 0) FAIL("Did not call check_sanity");
 
 	if (H2K_thread_create((u32_t)test_thread,((u32_t)(&stack)),0xdeadbeef,2,0x0,&a) 

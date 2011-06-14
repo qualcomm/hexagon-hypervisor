7da0699e8258d32c0826475db324eea93550fcc1
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2011-06-14 12:11:06 -0500

    Comment out whole skipped test so that subsequent ones succeed.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- kernel/thread/create/test/test.c
+++ kernel/thread/create/test/test.c
@@ -123,12 +123,13 @@ int main()
 	/* asid count should have gone to 2 and then back to 1 */
 	if (H2K_mem_asid_table[asid].count != 1) FAIL("Bad asid count");
 	if (ret != -1) FAIL("Exceeded max_cpus");
-	vm.num_cpus = 1;
 
-	ret = H2K_thread_create_no_squash(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,4,&vm,&a);
+	vm.num_cpus = 1;
 #warning Need to reenable this test and fix when audio becomes unbroken
+	//ret = H2K_thread_create_no_squash(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,4,&vm,&a);
 	//if (ret != -1) FAIL("Exceeded vm bestprio");
 
+	/* should succeed */
 	ret = H2K_thread_create_no_squash(((u32_t)test_thread),((u32_t)(&stack)),0xdeadbeef,6,&vm,&a);
 	if (ret == -1) FAIL("Unexpected error");
 

b00746c0be55bd79178a845a5841d55b2b0e342e
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2011-06-21 17:23:31 -0500

    Save guest IE status in GSSR and disable.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- kernel/vm/vmevent/vmevent.ref.c
+++ kernel/vm/vmevent/vmevent.ref.c
@@ -42,10 +42,19 @@ void H2K_vm_event(u32_t gbadva, u32_t cause, u32_t vec_offset, H2K_thread_contex
 		me->ssr |= (1<<SSR_GUEST_BIT);
 		me->r29 = me->gosp;
 		me->gosp = tmp;
-		me->gssr = 0x80000000 | cause;
+		me->gssr = H2K_GSSR_UM | cause;
 	} else {
 		me->gssr = cause;
 	}
+	/* save IE status and disable */
+	if (me->vmstatus & H2K_VMSTATUS_IE) {
+		me->gssr |= H2K_GSSR_IE;
+	}
+	else {
+		me->gssr &= ~H2K_GSSR_IE;
+	}
+
+	H2K_disable_guest_interrupts(me);
 	H2K_mem_event_restore_gregs(me);
 }
 

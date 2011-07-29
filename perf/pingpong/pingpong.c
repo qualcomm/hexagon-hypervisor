c57d4576eea05cf766744b60df8f0cf2cfa9615e
Erich Plondke <erich@qti.qualcomm.com>
2011-07-29 15:34:31 -0500

    pingpong makes qurt stuff too
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

--- perf/pingpong/pingpong.c
+++ perf/pingpong/pingpong.c
@@ -3,12 +3,12 @@
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
-#include <h2.h>
+#include <blast.h>
 #include <stdio.h>
 
-h2_sem_t toping;
-h2_sem_t topong;
-h2_sem_t tomain;
+blast_sem_t toping;
+blast_sem_t topong;
+blast_sem_t tomain;
 
 unsigned long long int ping_stack[64];
 unsigned long long int pong_stack[64];
@@ -16,52 +16,81 @@ unsigned long long int pong_stack[64];
 #ifdef DEBUG
 #define ITERS 10
 #else
-#define ITERS (1000*1000)
+#define ITERS (100*1000)
+#endif
+
+#ifdef H2_H
+static inline void my_thread_create(void *f, void *s, int ss, void *p, int prio) { h2_thread_create(f,s,p,prio); }
+#else
+static inline void my_thread_create(void *f, void *s, int ss, void *p, int prio) { 
+	qurt_thread_attr_t attr;
+	qurt_thread_t tid;
+	int ret;
+	char tname[16];
+	sprintf(tname, "T%08x",(int)p);
+
+	//printf( "thread %d started\n", id );
+	qurt_thread_attr_set_name(&attr, (char*)tname);
+    
+	qurt_thread_attr_init (&attr);
+	qurt_thread_attr_set_stack_size (&attr, ss);
+	qurt_thread_attr_set_stack_addr (&attr, s);
+	qurt_thread_attr_set_priority (&attr, prio);
+	/* TIMETEST IDs 0 - 31 are reserved for interrrupts */
+	qurt_thread_attr_set_timetest_id (&attr, prio);
+
+	ret = qurt_thread_create(&tid, &attr, f, (void *)p);
+	if (ret == -1) {
+		printf(" failed to create thread \n");
+	}
+}
 #endif
 
 void ping(void *unused) {
 	int i;
-	h2_sem_up(&tomain);
+	blast_sem_up(&tomain);
 	for (i = 0; i < ITERS; i++) {
-		h2_sem_down(&toping);
+		blast_sem_down(&toping);
 		#ifdef DEBUG
 		printf("ping!\n");
 		#endif
-		h2_sem_up(&topong);
+		blast_sem_up(&topong);
 	}
-	h2_sem_up(&tomain);
-	h2_thread_stop();
+	blast_sem_up(&tomain);
+	blast_thread_stop();
 }
 
 void pong(void *unused) {
-	h2_sem_up(&tomain);
+	blast_sem_up(&tomain);
 	while (1) {
-		h2_sem_down(&topong);
+		blast_sem_down(&topong);
 		#ifdef DEBUG
 		printf("pong!\n");
 		#endif
-		h2_sem_up(&toping);
+		blast_sem_up(&toping);
 	}
-	h2_thread_stop();
+	blast_thread_stop();
 }
 
 char context_space[1024];
 
 int main() {
 	unsigned long long int start,end;
+#ifdef H2_H
 	h2_init(NULL);
 	h2_config_add_thread_storage(context_space,sizeof(context_space));
-	h2_sem_init_val(&toping,0);
-	h2_sem_init_val(&topong,0);
-	h2_sem_init_val(&tomain,0);
-	h2_thread_create((void *)ping,&ping_stack[63],NULL,254);
-	h2_thread_create((void *)pong,&pong_stack[63],NULL,254);
-	h2_sem_down(&tomain);
-	h2_sem_down(&tomain);
-	start = h2_get_core_pcycles();
-	h2_sem_up(&toping);
-	h2_sem_down(&tomain);
-	end = h2_get_core_pcycles();
+#endif
+	blast_sem_init_val(&toping,0);
+	blast_sem_init_val(&topong,0);
+	blast_sem_init_val(&tomain,0);
+	my_thread_create((void *)ping,&ping_stack[63],64*8,NULL,254);
+	my_thread_create((void *)pong,&pong_stack[63],64*8,NULL,254);
+	blast_sem_down(&tomain);
+	blast_sem_down(&tomain);
+	start = blast_get_core_pcycles();
+	blast_sem_up(&toping);
+	blast_sem_down(&tomain);
+	end = blast_get_core_pcycles();
 	printf("Done! cycles=%lld\n", end-start);
 }
 

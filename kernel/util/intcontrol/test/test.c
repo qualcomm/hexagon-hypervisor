d74e58864319453f5f7dcd80f384bdd68a3f7683
Erich Plondke <erich@qti.qualcomm.com>
2011-08-24 17:05:13 -0500

    fix v3 for intcontrol tests
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

--- kernel/util/intcontrol/test/test.c
+++ kernel/util/intcontrol/test/test.c
@@ -4,7 +4,10 @@
  */
 
 #include <c_std.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <intcontrol.h>
+#include <q6protos.h>
 
 void FAIL(const char *str)
 {
@@ -29,6 +32,9 @@ static inline u32_t read_iad()
 {
 	u32_t ret;
 	asm volatile ("%0 = iad" : "=r"(ret));
+#if ARCHV <= 3
+	ret = Q6_R_brev_R(ret);
+#endif
 	return ret;
 }
 
@@ -36,8 +42,10 @@ int main()
 {
 	int i,l2i;
 	__asm__ __volatile(" r16 = %0 " : : "r"(&H2K_kg));
+#if ARCHV >= 4
 	H2K_gp->l2_int_base = fake_intctrl;
 	H2K_gp->l2_ack_base = fake_intctrl+0x200/sizeof(u32_t);
+#endif
 	for (i = 0; i < MAX_INTERRUPTS; i++) {
 		if ((ARCHV >= 4) && (i == 31)) continue;
 		if (i < 32) {
@@ -48,11 +56,15 @@ int main()
 			H2K_intcontrol_enable_TB(i);
 			if (0 != (1 & (read_iad() >> i))) FAIL("Didn't enable L1");
 		} else {
+#if ARCHV > 3
 			l2i = i-32;
 			H2K_intcontrol_disable_TB(i);
 			if (0 == (1 & (H2K_gp->l2_int_base[0x60+l2i/32] >> (i % 32)))) FAIL("didn't disable");
 			H2K_intcontrol_enable_TB(i);
 			if (0 == (1 & (H2K_gp->l2_int_base[0x80+l2i/32] >> (i % 32)))) FAIL("didn't enable");
+#else
+			FAIL("Should only be 32 interrupts on V3 and prior");
+#endif
 		}
 	}
 

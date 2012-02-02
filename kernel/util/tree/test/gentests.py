d462a40b2e2490efefa6673cc55062c82920e00a
Erich Plondke <erich@qti.qualcomm.com>
2012-02-02 14:34:42 -0600

    make smaller
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

--- kernel/util/tree/test/gentests.py
+++ kernel/util/tree/test/gentests.py
@@ -5,7 +5,8 @@ import random
 
 f = open('generated_tests.dat','w')
 
-SIZE = 1000
+SIZE = 700
+N = 15
 
 NAME = "big_inorder"
 MIN = 0
@@ -17,7 +18,7 @@ MIN = 1
 MAX = SIZE
 print >>f, "TEST(%s,%d,%d,%d,%s)" % (NAME,SIZE,MIN,MAX,"{ %s }" % (",".join([ str(x) for x in range(SIZE,0,-1)])))
 
-for i in range(30):
+for i in range(N):
 	NAME = "big_unique_random_%d" % i
 	arr = range(SIZE)
 	random.shuffle(arr)
@@ -25,7 +26,7 @@ for i in range(30):
 	MAX = max(arr)
 	print >>f, "TEST(%s,%d,%d,%d,%s)" % (NAME,SIZE,MIN,MAX,"{ %s }" % (",".join([ str(x) for x in arr])))
 
-for i in range(30):
+for i in range(N):
 	NAME = "big_nonunique_random_%d" % i
 	arr = [ random.randrange(SIZE) for i in range(SIZE) ]
 	MIN = min(arr)

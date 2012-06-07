886cd9c695878a7e22eb828a9fbcd9183c7d48fc
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2012-06-07 15:15:03 -0500

    Manipulate H2K_mem_asid_table counts atomically.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- kernel/util/atomic/atomic.h
+++ kernel/util/atomic/atomic.h
@@ -51,6 +51,24 @@ static inline u32_t H2K_atomic_swap(u32_t *word, u32_t val)
 	return t;
 }
 
+static inline int H2K_atomic_compare_swap(u32_t *word, u32_t expect, u32_t val)
+{
+	u32_t t;
+
+	asm volatile( "// atomic compare and swap\n"
+		"1:	%0 = memw_locked(%2);\n"
+		"	{ p0 = cmp.eq(%0, %3);\n"
+		"	  if (!p0.new) jump:nt 2f; }\n"
+		"	memw_locked(%2, p0) = %4;\n"
+		"	if (!p0) jump 1b;\n"
+		"2:\n"
+		: "=&r"(t), "+m"(*word)
+		: "r" (word), "r" (expect), "r" (val)
+		: "p0"
+	);
+	return t;
+}
+
 static inline u32_t H2K_atomic_insert(u32_t *word, u32_t val, u32_t width, u32_t offset)
 {
 	u32_t t;
@@ -71,6 +89,22 @@ static inline u32_t H2K_atomic_insert(u32_t *word, u32_t val, u32_t width, u32_t
 		: "=&r"(t),"+m"(*word)
 		: "r"(val),"r"(word),"r"(x.insert_info)
 		: "p0");
+	return t;
+}
+
+static inline u32_t H2K_atomic_add(u32_t *word, u32_t val) {
+
+	u32_t t;
+
+	asm ("// atomic add\n"
+			 "1: %0 = memw_locked(%3)\n"
+			 "   %0 = add(%0, %2)\n"
+			 "   memw_locked(%3, p0) = %0\n"
+			 "   if !p0 jump 1b\n"
+			 : "=&r"(t),"+m"(*word)
+			 : "r"(val),"r"(word)
+			 : "p0");
+
 	return t;
 }
 

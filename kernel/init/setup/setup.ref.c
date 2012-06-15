ae6960ccc32645b3344ee149e904e013ebaefe36
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2012-06-15 13:57:04 -0500

    Pass phys offset to setup for boot vm pmap; temp fix for loadlinux.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- kernel/init/setup/setup.ref.c
+++ kernel/init/setup/setup.ref.c
@@ -32,18 +32,18 @@ static char vmblock_space[VMBLOCK_SIZE(MAX_BOOT_CONTEXTS, INTS_PER_BOOT_CONTEXT)
 
 H2K_vmblock_t *bootvm;
 
-void H2K_init_setup_bootvm()
+void H2K_init_setup_bootvm(u32_t phys_offset)
 {
 	BKL_UNLOCK();  // because config locks
 	bootvm = (H2K_vmblock_t *)H2K_trap_config(CONFIG_VMBLOCK_INIT, vmblock_space, SET_STORAGE, 0, 0, NULL);
 	H2K_gp->vmblocks[1] = bootvm;
 
-	H2K_trap_config(CONFIG_VMBLOCK_INIT, bootvm, SET_PMAP_TYPE, (u32_t)H2K_linear_bootmap, H2K_ASID_TRANS_TYPE_LINEAR, NULL);
+	H2K_trap_config(CONFIG_VMBLOCK_INIT, bootvm, SET_PMAP_TYPE, (u32_t)H2K_linear_bootmap - phys_offset, H2K_ASID_TRANS_TYPE_LINEAR, NULL);
 	H2K_trap_config(CONFIG_VMBLOCK_INIT, bootvm, SET_PRIO_TRAPMASK, 0, 0xffffffff, NULL);
 	H2K_trap_config(CONFIG_VMBLOCK_INIT, bootvm, SET_CPUS_INTS, MAX_BOOT_CONTEXTS, INTS_PER_BOOT_CONTEXT, NULL);
 }
 
-IN_SECTION(".text.init.setup") void H2K_init_setup()
+IN_SECTION(".text.init.setup") void H2K_init_setup(u32_t phys_offset)
 {
 	H2K_kg_init();		/* Kernel Globals first! */
 	H2K_trace_init();
@@ -57,14 +57,14 @@ IN_SECTION(".text.init.setup") void H2K_init_setup()
 	H2K_asid_table_init();
 	H2K_mem_stlb_init();
 	H2K_timer_init();
-	H2K_init_setup_bootvm();
+	H2K_init_setup_bootvm(phys_offset);
 }
 
-IN_SECTION(".text.init.boot") void H2K_thread_boot()
+IN_SECTION(".text.init.boot") void H2K_thread_boot(u32_t phys_offset)
 {
 	s32_t asid;
 
-	H2K_init_setup();
+	H2K_init_setup(phys_offset);
 
 	/* allocate first thread */
 	H2K_thread_context *boot = bootvm->free_threads;

0c216fe7be8d902cbeddc6f23dcec1fc13383dac
Erich Plondke <erich@qti.qualcomm.com>
2012-07-13 15:11:49 -0500

    ping in a circle instead of pingpong that can be short circuited
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

--- perf/pingpong/pingpong.c
+++ perf/pingpong/pingpong.c
@@ -6,12 +6,12 @@
 #include <blast.h>
 #include <stdio.h>
 
-blast_sem_t toping;
-blast_sem_t topong;
+#define MAX_THREADS 8
+
+blast_sem_t pingsem[MAX_THREADS];
 blast_sem_t tomain;
 
-unsigned long long int ping_stack[64];
-unsigned long long int pong_stack[64];
+unsigned long long int stacks[MAX_THREADS][64];
 
 #ifdef DEBUG
 #define ITERS 10
@@ -46,29 +46,23 @@ static inline void my_thread_create(void *f, void *s, int ss, void *p, int prio)
 }
 #endif
 
-void ping(void *unused) {
+void ping(void *id) {
 	int i;
+	int myid = (int)id;
+	printf("Ping thread %d\n",myid);
+	blast_sem_t *in, *out;
+	in = &pingsem[myid];
+	if (myid == (MAX_THREADS-1)) out = &pingsem[0];
+	else out = &pingsem[myid+1];
 	blast_sem_up(&tomain);
 	for (i = 0; i < ITERS; i++) {
-		blast_sem_down(&toping);
+		blast_sem_down(in);
 		#ifdef DEBUG
-		printf("ping!\n");
+		printf("%d: ping!\n",myid);
 		#endif
-		blast_sem_up(&topong);
+		blast_sem_up(out);
 	}
-	blast_sem_up(&tomain);
-	blast_thread_stop();
-}
-
-void pong(void *unused) {
-	blast_sem_up(&tomain);
-	while (1) {
-		blast_sem_down(&topong);
-		#ifdef DEBUG
-		printf("pong!\n");
-		#endif
-		blast_sem_up(&toping);
-	}
+	if (myid == MAX_THREADS-1) blast_sem_up(&tomain);
 	blast_thread_stop();
 }
 
@@ -76,17 +70,17 @@ char context_space[1024];
 
 int main() {
 	unsigned long long int start,end;
-	blast_sem_init_val(&toping,0);
-	blast_sem_init_val(&topong,0);
+	int i;
 	blast_sem_init_val(&tomain,0);
-	my_thread_create((void *)ping,&ping_stack[64],64*8,NULL,254);
-	my_thread_create((void *)pong,&pong_stack[64],64*8,NULL,254);
-	blast_sem_down(&tomain);
-	blast_sem_down(&tomain);
+	for (i = 0; i < MAX_THREADS; i++) {
+		blast_sem_init_val(&pingsem[i],0);
+		my_thread_create(ping,&stacks[i][64],64*8,(void *)i,100);
+		blast_sem_down(&tomain);
+	}
 	start = blast_get_core_pcycles();
-	blast_sem_up(&toping);
+	blast_sem_up(&pingsem[0]);
 	blast_sem_down(&tomain);
 	end = blast_get_core_pcycles();
-	printf("TEST PASSED - %.0f\n", (float) (end - start) / (float) ITERS);
+	printf("TEST PASSED - %.0f\n", (float) (end - start) / (float) (ITERS*MAX_THREADS));
 	return 0;
 }

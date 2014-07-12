28e8fac1be9b9746123ba43ebb4986ae3bc740d3
Erich Plondke <erich@qti.qualcomm.com>
2014-07-11 23:50:08 -0500

    other sleeping functions as inlines... why not?
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

--- libs/h2/sleep/h2_sleep.h
+++ libs/h2/sleep/h2_sleep.h
@@ -7,6 +7,9 @@
 #define H2_SLEEP_H 1
 
 unsigned long long int h2_nanosleep(unsigned long long int time);
+static inline unsigned long long int h2_microsleep(unsigned long long int time) { return h2_nanosleep(time*1000)/1000; }
+static inline unsigned long long int h2_millisleep(unsigned long long int time) { return h2_microsleep(time*1000)/1000; }
+static inline unsigned long long int h2_sleep(unsigned long long int time) { return h2_millisleep(time*1000)/1000; }
 
 #endif
 

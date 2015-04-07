c6b9cbabd585198063b57a72f038ee16be4dbbdf
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2015-04-07 15:20:07 -0500

    Fix setup test.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- kernel/init/setup/test/test.c
+++ kernel/init/setup/test/test.c
@@ -90,7 +90,7 @@ HELPER_FUNC(l2cache_init)
 //HELPER_FUNC(asid_table_init)
 //HELPER_FUNC(mem_stlb_init)
 
-void H2K_kg_init(u32_t phys_offset, u32_t devpage_offset, u32_t last_tlb_index) { TH_init_seen |= 1<< kg_init; }
+void H2K_kg_init(u32_t phys_offset, u32_t devpage_offset, u32_t last_tlb_index, u32_t tlb_size) { TH_init_seen |= 1<< kg_init; }
 
 extern H2K_vmblock_t *bootvm;
 

14cd037dbd0d8d17e68b4584231fab2f98fb1188
Erich Plondke <erich@qti.qualcomm.com>
2015-05-25 23:51:30 -0500

    intconfig changes to match l2vic on 8909 modem
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

--- kernel/data/intconfig/intconfig.ref.c
+++ kernel/data/intconfig/intconfig.ref.c
@@ -70,115 +70,46 @@ static void H2K_intconfig_l2_init(ssbase)
 	int i;
 	volatile unsigned int *intbase = H2K_gp->l2_int_base;
 
-	u32_t spi_irq, spi_word, tlmm_irq, tlmm_word;
-
-	//	u32_t hexagon_ipc_irq[4];   //  hardcoded number good, so good.  taste so rite.
-	//	u32_t hexagon_ipc_word[4];
-
-#if ARCHV >= 5
-	u32_t spmi_arb_periph_irq, spmi_arb_periph_word, spmi_arb_ee_irq, spmi_arb_ee_word;
-#endif
-
-#if ARCHV == 4
-	/*  8960 SPI and TLMM summary apparently are level (high) triggered */
-#define SPI_IRQ		65
-#define SPI_WORD	(SPI_IRQ/32)
-#define TLMM_IRQ	38
-#define TLMM_WORD	(TLMM_IRQ/32)
-
-	spi_irq = SPI_IRQ;
-	spi_word = SPI_WORD;
-	tlmm_irq = TLMM_IRQ;
-	tlmm_word = TLMM_WORD;
-#endif
-
-#if ARCHV >= 5
-#define MSS_SPI_IRQ		245
-#define MSS_SPI_WORD	(MSS_SPI_IRQ/32)
-#define MSS_TLMM_IRQ	239
-#define MSS_TLMM_WORD	(MSS_TLMM_IRQ/32)
-
-#define MSS_SPMI_ARB_PERIPH_IRQ	75
-#define MSS_SPMI_ARB_PERIPH_WORD	(MSS_SPMI_ARB_PERIPH_IRQ/32)
-#define MSS_SPMI_ARB_EE_IRQ		76
-#define MSS_SPMI_ARB_EE_WORD	(MSS_SPMI_ARB_EE_IRQ/32)
-
-		/*  8974 SPI ("blsp1_qup1") and "summary_irq_sensors"  */
-#define LPASS_SPI_IRQ		64
-#define LPASS_SPI_WORD	(LPASS_SPI_IRQ/32)
-#define LPASS_TLMM_IRQ	38
-#define LPASS_TLMM_WORD	(LPASS_TLMM_IRQ/32)
-
-#define LPASS_SPMI_ARB_PERIPH_IRQ	48
-#define LPASS_SPMI_ARB_PERIPH_WORD	(LPASS_SPMI_ARB_PERIPH_IRQ/32)
-#define LPASS_SPMI_ARB_EE_IRQ		49
-#define LPASS_SPMI_ARB_EE_WORD	(LPASS_SPMI_ARB_EE_IRQ/32)
-//#define LPASS_USB1_HS_IRQ	44
-#define LPASS_USB1_HS_IRQ	54
-#define LPASS_USB1_HS_WORD	(LPASS_USB1_HS_IRQ/32)
-
-#define LPASS_IPC_0_IRQ		18
-#define LPASS_IPC_0_WORD	(LPASS_IPC_0_IRQ/32)
-#define LPASS_IPC_1_IRQ		8
-#define LPASS_IPC_1_WORD	(LPASS_IPC_1_IRQ/32)
-#define LPASS_IPC_2_IRQ		33
-#define LPASS_IPC_2_WORD	(LPASS_IPC_2_IRQ/32)
-#define LPASS_IPC_3_IRQ		46
-#define LPASS_IPC_3_WORD	(LPASS_IPC_3_IRQ/32)
-
-	/* Ew ew ew */
-	if (ssbase == QDSP6SS_PRIV_BASE_MSS - QDSP6SS_PUB_PRIV_OFFSET) {
-		spi_irq = MSS_SPI_IRQ;
-		spi_word = MSS_SPI_WORD;
-		tlmm_irq = MSS_TLMM_IRQ;
-		tlmm_word = MSS_TLMM_WORD;
-		spmi_arb_periph_irq = MSS_SPMI_ARB_PERIPH_IRQ;
-		spmi_arb_periph_word = MSS_SPMI_ARB_PERIPH_WORD;
-		spmi_arb_ee_irq = MSS_SPMI_ARB_EE_IRQ;
-		spmi_arb_ee_word = MSS_SPMI_ARB_EE_WORD;
-	
-	} else {  // guess LPASS
-		spi_irq = LPASS_SPI_IRQ;
-		spi_word = LPASS_SPI_WORD;
-		tlmm_irq = LPASS_TLMM_IRQ;
-		tlmm_word = LPASS_TLMM_WORD;
-		spmi_arb_periph_irq = LPASS_SPMI_ARB_PERIPH_IRQ;
-		spmi_arb_periph_word = LPASS_SPMI_ARB_PERIPH_WORD;
-		spmi_arb_ee_irq = LPASS_SPMI_ARB_EE_IRQ;
-		spmi_arb_ee_word = LPASS_SPMI_ARB_EE_WORD;
-	}
-#endif
+	/* EJP: FIXME: hard coded values from qurt_config for 8909.  Need to put in api, I guess? */
+	const unsigned int l2_edgevals[] = {
+		0xf8ffe3f3L,
+		0x1f8fd1ffL,
+		0xee3fd21fL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0x1ff87c0L,
+		0x5fc33e00L,
+		0xffffffb7L,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL,
+		0xffffffffL};
 
 	for (i = 0; i < ((MAX_INTERRUPTS-32)/32); i++) {
-		intbase[(0x100/4) + i] = 0x0; 		/* DISABLED */
-		intbase[(0x280/4) + i] = 0xFFFFFFFF;	/* EDGE/level TRIGGERED */
-		intbase[(0x300/4) + i] = 0x0;		/* Rising Edge / Level High */
-		intbase[(0x400/4) + i] = 0xFFFFFFFF;	/* Interrupt Clear */
-
-		/*  is there a "clear bit" around here?  */
-		/*  There's probably a better way to do this.  */
-		if (i == spi_word) {
-			intbase[(0x280/4) + i] &= ~(1<<(spi_irq % 32));
-		}
-		if (i == tlmm_word) {
-			intbase[(0x280/4) + i] &= ~(1<<(tlmm_irq % 32));
-		}
-#if ARCHV >= 5
-		if (i == spmi_arb_periph_word) {
-			intbase[(0x280/4) + i] &= ~(1<<(spmi_arb_periph_irq % 32));
-		}
-		if (i == spmi_arb_ee_word) {
-			intbase[(0x280/4) + i] &= ~(1<<(spmi_arb_ee_irq % 32));
-		}
-
-		/* Ew ew ew, part deux */
-		if (ssbase != QDSP6SS_PRIV_BASE_MSS - QDSP6SS_PUB_PRIV_OFFSET) {
-			if (i == LPASS_USB1_HS_WORD) {
-				intbase[(0x280/4) + i] &= ~(1<<(LPASS_USB1_HS_IRQ % 32));
-			}
-		}
-
-#endif
+		intbase[(0x100/4) + i] = 0x0; 			/* DISABLED */
+		intbase[(0x280/4) + i] = l2_edgevals[i];	/* EDGE/level TRIGGERED */
+		intbase[(0x300/4) + i] = 0x0;			/* Rising Edge / Level High */
+		intbase[(0x400/4) + i] = 0xFFFFFFFF;		/* Interrupt Clear */
 	}
 	ciad(0x80000000);				/* Enable L2 Interrupts */
 }

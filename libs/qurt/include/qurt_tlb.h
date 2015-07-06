4f5b23d36b1de2c6f23d283443376a60af4c71a6
Erich Plondke <erich@qti.qualcomm.com>
2015-07-06 11:14:13 -0500

    fix sign extention error
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

--- libs/qurt/include/qurt_tlb.h
+++ libs/qurt/include/qurt_tlb.h
@@ -57,7 +57,7 @@ static inline h2_u64_t qurt_tlb_from_entry(qurt_addr_t vaddr, qurt_paddr_64_t pa
 	h2_u64_t entry;
 	entry = 0xC0000000 | (vaddr >> 12);
 	entry <<= 32;
-	entry |= perms << 28;
+	entry |= ((h2_u64_t)perms) << 29;
 	entry |= cache_attribs << 24;
 	entry |= ((paddr_64 >> 11) & (0x00FFFFFF) & (-2 << (size*2)));
 	entry |= (1<<(size));

4d6c05bd20c71a9ad2555f9062dd9eb64ed07572
Erich Plondke <erich@qti.qualcomm.com>
2015-11-04 09:39:15 -0600

    oops, forgot file
    Signed-off-by: Erich Plondke <erich@qti.qualcomm.com>

---
+++ libs/h2/common/h2_common_asm_std.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#ifndef H2K_ASM_STD_H
+#define H2K_ASM_STD_H 1
+
+#ifndef ASM
+#define ASM 1
+#endif
+
+#define H2K_GP R28
+
+#define NULL 0
+
+#define H2K_LANG_IS_ASM 1
+
+.macro FUNC_START name, section, align, amt
+	.section \section,"ax",@progbits
+	\align \amt
+	.global \name
+	.type \name,@function
+\name:
+.endm
+
+.macro FUNC_END name
+	.size \name,.-\name
+.endm
+
+.macro DATA_START name, section, align, amt
+	.section \section,"aw",@progbits
+	\align \amt
+	.global \name
+	.type \name,@object
+\name:
+.endm
+
+.macro DATA_END name
+	.size \name,.-\name
+.endm
+
+#endif
+

4ad7985fdaec4b52d6c2affdc59040e022ed50f2
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2016-03-24 22:11:51 -0500

    Add h2_galloc.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- libs/h2/alloc/test/test.c
+++ libs/h2/alloc/test/test.c
@@ -19,17 +19,59 @@ void FAIL(const char *str)
 int main() 
 {
 	void *ptr;
+	void *tcm;
+
+	h2_galloc_t alloc0, alloc1;
+
 //	h2_init(0x0);
 
 	ptr = h2_memalign(32,100);
 
 	info("ptr_value = 0x%08x\n",ptr);
 	if (ptr == NULL) {
-		error("FAIL\n");
+		error("memalign\n");
 	}
-	else {
-		info("TEST PASSED\n");
+
+	tcm = (void *)h2_info(INFO_TCM_BASE);
+
+	h2_galloc_init(&alloc0, (unsigned int)tcm, 1000, &alloc1);
+	h2_galloc_init(&alloc1, (unsigned int)tcm + 1000, 2000, NULL);
+
+	ptr = h2_galloc(&alloc0, 100, 0);
+	if (ptr != tcm) {
+		error("galloc 1\n");
 	}
+
+	ptr = h2_galloc(&alloc0, 901, 0);
+	if (NULL != ptr) {
+		error("galloc 2\n");
+	}
+	
+	ptr = h2_galloc(&alloc0, 901, 1);
+	if ((unsigned int)ptr != (unsigned int)tcm + 1000) {
+		error("galloc 3\n");
+	}
+
+	h2_galloc_reset(&alloc0, 0);
+	if (alloc0.cur != alloc0.base) {
+		error("galloc 4\n");
+	}
+	if (alloc1.cur == alloc1.base) {
+		error("galloc 5\n");
+	}
+
+	h2_galloc_reset(&alloc0, 1);
+	if (alloc1.cur != alloc1.base) {
+		error("galloc 6\n");
+	}
+
+	ptr = h2_galloc(&alloc0, 3000, 1);
+	if (NULL != ptr) {
+		error("galloc 7\n");
+	}
+
+	info("TEST PASSED\n");
+
 	h2_thread_stop(0);
 	return(0);
 }

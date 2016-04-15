00cb86d1263734441d300f5542a867196e9799e8
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2016-04-15 14:17:34 -0500

    Bring back __h2_handle_errors_hook__().
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- libs/syscall/angel/src/sys_get_cmdline.c
+++ libs/syscall/angel/src/sys_get_cmdline.c
@@ -4,6 +4,11 @@
  */
 
 #include "allsyscalls.h"
+#include <h2_error.h>
+
+void __attribute__ ((weak)) __h2_handle_errors_hook__(void) {
+	h2_handle_errors(0);
+}
 
 char __boot_cmdline__[256] __attribute__((section(".data"))) = { 0 };
 
@@ -11,6 +16,11 @@ errno_t sys_get_cmdline(char *buffer, count_t count)
 {
 	errno_t ret;
 	struct { char *buf; count_t count; } x;
+
+	if (0xfffffff0 != __h2_handle_errors_hook__) {
+		__h2_handle_errors_hook__();
+	}
+
 	if (__boot_cmdline__[0] != 0) {
 		strncpy(buffer,__boot_cmdline__,count);
 		buffer[count-1] = 0;

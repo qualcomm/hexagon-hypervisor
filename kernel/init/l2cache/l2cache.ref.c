a80d74428fe3d3c07c5af291b114e3fee2770cf2
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2017-08-29 12:58:48 -0500

    Add l2_v65_5.
    (cherry picked from commit 70e77ddaac020d6ca17060fc93a1cd9e4ca5e538)
    
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- kernel/init/l2cache/l2cache.ref.c
+++ kernel/init/l2cache/l2cache.ref.c
@@ -680,6 +680,26 @@ static u8_t l2_v65_2[] =
 		[0xf] = reserved
 	};
 
+static u8_t l2_v65_5[] =
+	{
+		[0x0] = s0,
+		[0x1] = reserved,
+		[0x2] = reserved,
+		[0x3] = reserved,
+		[0x4] = s512,
+		[0x5] = reserved,
+		[0x6] = reserved,
+		[0x7] = reserved,
+		[0x8] = reserved,
+		[0x9] = reserved,
+		[0xa] = reserved,
+		[0xb] = reserved,
+		[0xc] = reserved,
+		[0xd] = reserved,
+		[0xe] = reserved,
+		[0xf] = reserved
+	};
+
 static u8_t l2_v65_6[] =
 	{
 		[0x0] = s0,
@@ -1049,7 +1069,7 @@ static u8_t *uarches_v65[] =
 		[0x2] = l2_v65_2,
 		[0x3] = NULL,
 		[0x4] = NULL,
-		[0x5] = NULL,
+		[0x5] = l2_v65_5,
 		[0x6] = l2_v65_6,
 		[0x7] = l2_v65_7,
 		[0x8] = l2_v65_8,

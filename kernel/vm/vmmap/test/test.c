a03aa554b4f8dcb5980eb656e470b1a1dc59919b
Anil Govindaraju <angovind@qti.qualcomm.com>
2020-02-04 05:35:41 -0600

    revert vm vmmap test as reworking test of tlb mem invalidate va
    Signed-off-by: Anil Govindaraju <angovind@qti.qualcomm.com>

--- kernel/vm/vmmap/test/test.c
+++ kernel/vm/vmmap/test/test.c
@@ -12,7 +12,7 @@
 #include <stlb.h>
 #include <asid.h>
 #include <vmmap.h>
-#include <tmpmap.h>
+
 void FAIL(const char *str)
 {
 	puts("FAIL");
@@ -44,7 +44,7 @@ u32_t TH_saw_tlb_inv_va = 0;
 u32_t TH_oldasid = 0;
 u32_t TH_newasid = 0;
 
-void H2K_mem_stlb_invalidate_asid_ext(u32_t asid)
+void H2K_mem_stlb_invalidate_asid(u32_t asid)
 {
 	if (TH_expected_stlb_invasid == 0) FAIL("Unexpected invalidate stlb");
 	TH_expected_stlb_invasid = 0;
@@ -52,14 +52,14 @@ void H2K_mem_stlb_invalidate_asid_ext(u32_t asid)
 	TH_saw_stlb_invasid = 1;
 }
 
-void H2K_mem_stlb_invalidate_va_ext(u32_t va, u32_t count, u32_t asid, H2K_thread_context *me) {
+void H2K_mem_stlb_invalidate_va(u32_t va, u32_t count, u32_t asid, H2K_thread_context *me) {
 	if (TH_expected_stlb_inv_va == 0) FAIL("Unexpected invalidate stlb va");
 	TH_expected_stlb_inv_va = 0;
 	if (asid != TH_oldasid) FAIL("unexpected asid inv stlb");
 	TH_saw_stlb_inv_va = 1;
 }
 
-void H2K_mem_tlb_invalidate_asid_ext(u32_t asid)
+void H2K_mem_tlb_invalidate_asid(u32_t asid)
 {
 	if (TH_expected_tlb_invasid == 0) FAIL("Unexpected invalidate tlb");
 	TH_expected_tlb_invasid = 0;
@@ -67,7 +67,7 @@ void H2K_mem_tlb_invalidate_asid_ext(u32_t asid)
 	TH_saw_tlb_invasid = 1;
 }
 
-void H2K_mem_tlb_invalidate_va_ext(u32_t va, u32_t count, u32_t asid, H2K_thread_context *me) {
+void H2K_mem_tlb_invalidate_va(u32_t va, u32_t count, u32_t asid, H2K_thread_context *me) {
 	if (TH_expected_tlb_inv_va == 0) FAIL("Unexpected invalidate tlb va");
 	TH_expected_tlb_inv_va = 0;
 	if (asid != TH_oldasid) FAIL("unexpected asid inv tlb");
@@ -101,13 +101,7 @@ void H2K_asid_table_dec(u32_t asid)
 int main()
 {
 	__asm__ __volatile(GLOBAL_REG_STR " = %0 " : : "r"(&H2K_kg));
-	H2K_kg.last_tlb_index = 0x7d;
-	H2K_kg.tlb_size = 0x80;
-	u32_t asid;
-	asm volatile (
-	" %0 = ssr \n"
-	" %0 = extractu(%0,#7,#8)\n" 
-	: "=r"(asid));
+
 	a.vmblock = &av;
 
 	/* CLRMAP */
@@ -115,10 +109,6 @@ int main()
 	a.r00 = 0x1000;
 	a.r01 = 0;
 	H2K_vmtrap_clrmap(&a);
-	if (a.r01 != 0) {
-		H2K_mem_stlb_invalidate_va_ext(a.r00, a.r01, a.ssr_asid, &a);
-		H2K_mem_tlb_invalidate_va_ext(a.r00, a.r01, a.ssr_asid, &a);
-	}
 	if (TH_saw_stlb_invasid || TH_saw_tlb_invasid) FAIL("saw invalidate on 0 count");
 	if (a.r00 != 0) FAIL("clrmap ret");
 
@@ -127,29 +117,9 @@ int main()
 	a.r01 = 0x10000;
 	TH_expected_tlb_inv_va = TH_expected_stlb_inv_va = 1;
 	H2K_vmtrap_clrmap(&a);
-	if (a.r01 != 0) {
-		H2K_mem_stlb_invalidate_va_ext(a.r00, a.r01, a.ssr_asid, &a);
-		H2K_mem_tlb_invalidate_va_ext(a.r00, a.r01, a.ssr_asid, &a);
-	}
 	if (!(TH_saw_stlb_inv_va && TH_saw_tlb_inv_va)) FAIL("no invalidate");
 	if (a.r00 != 0) FAIL("clrmap ret");
 	TH_saw_stlb_inv_va = TH_saw_tlb_inv_va = 0;
-
-#if 0
-	TH_oldasid = a.ssr_asid = asid;
-	a.r00 = H2K_tmpmap_add_and_lock((pa_t)H2K_LINK_ADDR, UNCACHED);
-	a.r01 = 0x1;
-	TH_expected_tlb_inv_va = TH_expected_stlb_inv_va = 1;
-	H2K_vmtrap_clrmap(&a);
-	if (a.r01 != 0) {
-		H2K_mem_stlb_invalidate_va_ext(a.r00, a.r01, a.ssr_asid, &a);
-		H2K_mem_tlb_invalidate_va_ext(a.r00, a.r01, a.ssr_asid, &a);
-	}
-	H2K_tmpmap_remove_and_unlock();
-	if (!(TH_saw_stlb_inv_va && TH_saw_tlb_inv_va)) FAIL("no invalidate on 1 count");
-	if (a.r00 != 0) FAIL("clrmap ret");
-	TH_saw_stlb_inv_va = TH_saw_tlb_inv_va = 0;
-#endif
 
 	/* NEWMAP */
 

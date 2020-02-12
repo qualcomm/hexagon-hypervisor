89e7b067f7cd7cc74365baa9c21df5d6f3b21ad1
Anil Govindaraju <angovind@qti.qualcomm.com>
2020-02-12 02:37:23 -0600

    correct code to remove compiler optimization in mem tlbmisc test of invalidate va
    Signed-off-by: Anil Govindaraju <angovind@qti.qualcomm.com>

--- kernel/mem/tlbmisc/test/test.c
+++ kernel/mem/tlbmisc/test/test.c
@@ -15,7 +15,6 @@
 #include <globals.h>
 
 H2K_kg_t H2K_kg;
-H2K_thread_context H2K_tc;
 
 void FAIL(const char *str)
 {
@@ -26,8 +25,8 @@ void FAIL(const char *str)
 
 /* Helper functions */
 u32_t find_tlb_index_last_used(void) {
-    u32_t found_tlb_index = H2K_gp->last_tlb_index;  
-    for (u32_t index = 0; index < H2K_gp->last_tlb_index; index++) {
+    u32_t found_tlb_index = H2K_kg.last_tlb_index;  
+    for (u32_t index = 0; index < H2K_kg.last_tlb_index; index++) {
         u64_t entry = H2K_mem_tlb_read(index);
         if (entry != 0) {
             found_tlb_index = index;
@@ -37,56 +36,60 @@ u32_t find_tlb_index_last_used(void) {
 }
 
 void set_entry_at_tlb_index(u32_t tlb_index, u64_t tlb_entry, u32_t va, u32_t asid) {
-	H2K_mem_tlbfmt_t entry;
+    H2K_mem_tlbfmt_t entry;
     entry.raw = tlb_entry;
     entry.vpn = (va >> PAGE_BITS);
-	entry.asid = asid;
-	tlb_entry = entry.raw; // with updated va & asid
-	H2K_spinlock_lock(&H2K_gp->tmpmap_lock);  // for tmpmap
-	H2K_mutex_lock_tlb();  // for H2K_safemem
-	H2K_mem_tlb_write(tlb_index, tlb_entry);
-	H2K_isync();
-	H2K_mutex_unlock_tlb();
-	H2K_spinlock_unlock(&H2K_gp->tmpmap_lock);
+    entry.asid = asid;
+    tlb_entry = entry.raw; // with updated va & asid
+    H2K_spinlock_lock(&H2K_kg.tmpmap_lock);  // for tmpmap
+    H2K_mutex_lock_tlb();  // for H2K_safemem
+    H2K_mem_tlb_write(tlb_index, tlb_entry);
+    H2K_isync();
+    H2K_mutex_unlock_tlb();
+    H2K_spinlock_unlock(&H2K_kg.tmpmap_lock);
 }
 
 int main()
 {
     __asm__ __volatile(GLOBAL_REG_STR " = %0 " : : "r"(&H2K_kg));
     
-    H2K_gp->tlb_size = 128;
-    H2K_gp->last_tlb_index = H2K_kg.tlb_size - 1;
-    H2K_gp->pinned_tlb_mask = (~0ULL) << ((H2K_gp->last_tlb_index+1) & 0x3F);
+    H2K_kg.tlb_size = 128;
+    H2K_kg.last_tlb_index = H2K_kg.tlb_size - 1;
+    H2K_kg.pinned_tlb_mask = (~0ULL) << ((H2K_kg.last_tlb_index+1) & 0x3F);
     
-	u32_t tlb_index_last_used = find_tlb_index_last_used();
-	if (tlb_index_last_used >= H2K_gp->last_tlb_index) {
-		FAIL("TLB free index not found to attempt set and invalidate");
-	}
+    H2K_thread_context H2K_tc;
     
-	u64_t tlb_entry = H2K_mem_tlb_read(tlb_index_last_used);
-    
-    u32_t va = H2K_LINK_ADDR; //TODO - find better va
-    
-    u32_t asid = 0;
     __asm__ __volatile(
         " %0 = ssr \n"
         " %0 = extractu(%0,#7,#8)\n" 
-        : "=r"(asid));
+        : "=r"(H2K_tc.ssr_asid));
+    
+    u32_t tlb_index_last_used = find_tlb_index_last_used();
+    if (tlb_index_last_used >= H2K_kg.last_tlb_index) {
+        FAIL("TLB free index not found to attempt set and invalidate");
+    }
+    
+    u64_t tlb_entry = H2K_mem_tlb_read(tlb_index_last_used);
+    
+    u32_t va = H2K_LINK_ADDR; //TODO - find better va
+    
+    u32_t asid = H2K_tc.ssr_asid;
     
     set_entry_at_tlb_index(1+tlb_index_last_used, tlb_entry, va, asid);
-	
-	u32_t tlb_index = H2K_mem_tlb_probe(va, asid);
-	if (tlb_index >= H2K_gp->last_tlb_index) {
-		FAIL("TLB free index not set to attempt invalidate");
-	}
-	
-	H2K_mem_tlb_invalidate_va(va, 1, asid, &H2K_tc); // TBD
-	
-	u32_t tmp = H2K_mem_tlb_probe(va, asid);
-	if (tmp < H2K_gp->last_tlb_index) {
-		FAIL("TLB invalidate");
-	}
-	
+    
+    u32_t tlb_index = H2K_mem_tlb_probe(va, asid);
+    if (tlb_index >= H2K_kg.last_tlb_index) {
+        FAIL("TLB free index not set to attempt invalidate");
+    }
+    
+    H2K_mem_tlb_invalidate_va(va, 1, asid, &H2K_tc);
+    
+    asid = H2K_tc.ssr_asid;
+    tlb_index = H2K_mem_tlb_probe(va, asid);
+    if (tlb_index < H2K_kg.last_tlb_index) {
+        FAIL("TLB invalidate");
+    }
+    
     puts("TEST PASSED");
     return 0;
 }

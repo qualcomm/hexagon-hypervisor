0bd76b8861b08002bc2e30f36a7cebdb6aba2e4b
Anil Govindaraju <angovind@qti.qualcomm.com>
2020-04-02 11:14:13 -0500

    wip updates to unit test per review on use of runlist prio_hthreads previously unused in this unit
    Signed-off-by: Anil Govindaraju <angovind@qti.qualcomm.com>

--- kernel/data/runlist/test/test.c
+++ kernel/data/runlist/test/test.c
@@ -20,7 +20,7 @@ void FAIL(const char *str)
 }
 
 static H2K_thread_context a,b,c;
-static u32_t hthread_mask = 0;
+static u32_t hthread_mask;
 
 /* Create real versions of inlined functions */
 
@@ -34,18 +34,16 @@ s32_t H2K_runlist_worst_prio_hthread_TB()
 	return H2K_runlist_worst_prio_hthread();
 }
 
-u32_t H2K_runlist_push_TB(H2K_thread_context *thread)
+void H2K_runlist_push_TB(H2K_thread_context *thread)
 {
 	H2K_runlist_push(thread);
 	hthread_mask |= (0x1 << thread->hthread);
-	return hthread_mask;
 }
 
-u32_t H2K_runlist_remove_TB(H2K_thread_context *thread)
+void H2K_runlist_remove_TB(H2K_thread_context *thread)
 {
 	H2K_runlist_remove(thread);
 	hthread_mask &= ~(0x1 << thread->hthread);
-	return hthread_mask;
 }
 
 u32_t H2K_runlist_prio_hthreads_TB(u32_t hthreads, u32_t prio)
@@ -60,7 +58,7 @@ u32_t H2K_runlist_worst_prio_hthread_OK()
 	if (prio < 0 || prio > MAX_PRIO) {
 		return hthread < 0 || hthread > H2K_gp->hthreads;
 	}
-	return H2K_gp->runlist[hthread] && H2K_gp->runlist[hthread]->prio == prio;
+	return H2K_gp->runlist[hthread] != NULL && H2K_gp->runlist[hthread]->prio == prio;
 }
 
 int main() 
@@ -84,31 +82,38 @@ int main()
 	b.hthread = 1;
 	c.hthread = 2;
 
-	u32_t hthreads_c = H2K_runlist_push_TB(&c);
-	u32_t hthreads_zero = H2K_runlist_remove_TB(&c);
-	if (hthreads_zero != 0) FAIL("runlist_push_remove combo failed");
+	H2K_runlist_push_TB(&c);
+	u32_t hthreads_c = hthread_mask;
+	H2K_runlist_remove_TB(&c);
+	u32_t hthreads_none = hthread_mask;
+	if (hthreads_none != 0) FAIL("runlist push-remove combo failed");
 
-	u32_t hthreads_a = H2K_runlist_push_TB(&a);
+	H2K_runlist_push_TB(&a);
+	u32_t hthreads_a = hthread_mask;
 	if (H2K_gp->runlist[0] != &a) FAIL("runlist_push failed (0)");
 	if (H2K_gp->runlist_prios[0] != 2) FAIL("runlist_push failed (1)");
 	if (H2K_runlist_worst_prio_TB() != 2) FAIL("runlist_worst_prio failed (3)");
 	if (!H2K_runlist_worst_prio_hthread_OK()) FAIL("runlist_worst_prio_hthread failed (30)");
 	if (H2K_runlist_prio_hthreads_TB(hthreads_a,a.prio) != hthreads_a) FAIL("runlist_prio_hthreads failed (40)");
 
-	u32_t hthreads_ab = H2K_runlist_push_TB(&b);
+	H2K_runlist_push_TB(&b);
+	u32_t hthreads_ab = hthread_mask;
 	if (H2K_gp->runlist[1] != &b) FAIL("runlist_push failed (4)");
 	if (H2K_gp->runlist_prios[1] != 2) FAIL("runlist_push failed (5)");
 	if (H2K_runlist_worst_prio_TB() != 2) FAIL("runlist_worst_prio failed (7)");
 	if (!H2K_runlist_worst_prio_hthread_OK()) FAIL("runlist_worst_prio_hthread failed (31)");
 	if (H2K_runlist_prio_hthreads_TB(hthreads_ab,b.prio) != hthreads_ab) FAIL("runlist_prio_hthreads failed (41)");
 
-	u32_t hthreads_abc = H2K_runlist_push_TB(&c);
+	H2K_runlist_push_TB(&c);
+	u32_t hthreads_abc = hthread_mask;
 	if (H2K_runlist_worst_prio_TB() != 3) FAIL("runlist_worst_prio (8)");
 	if (!H2K_runlist_worst_prio_hthread_OK()) FAIL("runlist_worst_prio_hthread failed (32)");
 	if (H2K_runlist_prio_hthreads_TB(hthreads_abc,c.prio) != hthreads_c) FAIL("runlist_prio_hthreads failed (42)");
 
-	u32_t hthreads_bc = H2K_runlist_remove_TB(&a);
-	u32_t hthreads_b = H2K_runlist_remove_TB(&c);
+	H2K_runlist_remove_TB(&a);
+	u32_t hthreads_bc = hthread_mask;
+	H2K_runlist_remove_TB(&c);
+	u32_t hthreads_b = hthread_mask;
 	if (H2K_gp->runlist[0] != NULL) FAIL("runlist_remove failed (9)");
 	if (H2K_gp->runlist[1] != &b) FAIL("runlist_remove failed (10)");
 	if (H2K_gp->runlist[2] != NULL) FAIL("runlist_remove failed (11)");
@@ -119,11 +124,10 @@ int main()
 	if (H2K_runlist_prio_hthreads_TB(hthreads_bc,b.prio) != hthreads_bc) FAIL("runlist_prio_hthreads failed (43)");
 	if (H2K_runlist_prio_hthreads_TB(hthreads_b,b.prio) != hthreads_b) FAIL("runlist_prio_hthreads failed (44)");
 
-	u32_t hthreads_none = H2K_runlist_remove_TB(&b);
+	H2K_runlist_remove_TB(&b);
 	if (H2K_gp->runlist[1] != NULL) FAIL("runlist_remove failed (18)");
 	if (0 <= H2K_gp->runlist_prios[1] && H2K_gp->runlist_prios[1] <= MAX_PRIO) FAIL("runlist_remove failed (19)");
 	if (H2K_runlist_worst_prio_TB() <= MAX_PRIO) FAIL("runlist_worst_prio (21)");
-	if (hthreads_none != hthreads_zero) FAIL("runlist_hthreads none-zero comparison failed (45)");
 
 	puts("TEST PASSED\n");
 	return 0;

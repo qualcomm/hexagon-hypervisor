57c3a72e916e814a57d52b1fdbda408f9f88fd8e
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2021-01-11 13:11:53 -0600

    #ifdef out SYS_WRITE_BUF stuff.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- libs/syscall/angel/src/sys_write.c
+++ libs/syscall/angel/src/sys_write.c
@@ -5,6 +5,7 @@
 
 #include "allsyscalls.h"
 
+#ifdef SYS_WRITE_BUF
 extern char H2_ANGEL_write_buf[];
 extern unsigned int H2_ANGEL_write_buf_idx;
 extern const unsigned int H2_ANGEL_write_buf_size;
@@ -13,6 +14,7 @@ static inline void dccleana(const char *addr)
 {
 	asm volatile (" dccleana(%0)" : : "r"(addr));
 }
+#endif
 
 count_t sys_write(fd_t fd, const char *buffer, count_t count)
 {
@@ -24,6 +26,7 @@ count_t sys_write(fd_t fd, const char *buffer, count_t count)
 	clean(buffer,count/4+3);
 	clean(&x,3);
 	angel_ret = ANGEL(SYS_WRITE,&x,0);
+#ifdef SYS_WRITE_BUF
 	if (fd > 2) return angel_ret;
 	if (H2_ANGEL_write_buf_idx+count < H2_ANGEL_write_buf_size) {
 		memcpy(H2_ANGEL_write_buf+H2_ANGEL_write_buf_idx,buffer,count);
@@ -38,6 +41,7 @@ count_t sys_write(fd_t fd, const char *buffer, count_t count)
 			if (H2_ANGEL_write_buf_idx == H2_ANGEL_write_buf_size) H2_ANGEL_write_buf_idx = 0;
 		}
 	}
+#endif
 	return angel_ret;
 }
 

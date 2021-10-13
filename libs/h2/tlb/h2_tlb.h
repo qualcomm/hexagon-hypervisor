06563edd3a02f0082e3f9d1c77b43f1f47491f66
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2021-10-13 12:21:13 -0500

    Remove #if ARCHV in h2_tlb_dma_set().
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- libs/h2/tlb/h2_tlb.h
+++ libs/h2/tlb/h2_tlb.h
@@ -92,11 +92,7 @@ Set DMA TLB entry
 
 static inline int h2_tlb_dma_set(unsigned int index, unsigned long long int entry)
 {
-#if ARCHV >= 73
 	return (int)h2_tlb_trap(TLBOP_DMASET, index, entry);
-#else
-	return -1;
-#endif
 }
 
 #endif

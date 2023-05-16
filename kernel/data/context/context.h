42e73e51af59491cb01d78c27556471b1f636f8d
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2023-05-15 20:22:09 -0500

    Fix ssr_xe2 bit field.
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- kernel/data/context/context.h
+++ kernel/data/context/context.h
@@ -156,7 +156,7 @@ typedef struct _h2_thread_context
 					u8_t ssr_badva_pe:1;
 					u8_t ssr_bp:1;
 #ifdef HAVE_EXTENSIONS
-					u8_t ssr_xe2;
+					u8_t ssr_xe2:1;
 					u8_t ssr_xa:3;
 #else
 					u8_t ssr_hi_rsvd:4;

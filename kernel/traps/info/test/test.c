6ee19577a823a624030ac212fe0a91692e118d87
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2024-06-13 09:12:05 -0500

    popcount CFG_TABLE_HLX_CONTEXTS.
    (cherry picked from commit 6566c1182b93aaac12530949469c2d4e092898cd)
    
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- kernel/traps/info/test/test.c
+++ kernel/traps/info/test/test.c
@@ -139,10 +139,8 @@ int main() {
 	val = H2K_kg.coproc_contexts;
 	if (H2K_trap_info(INFO_COPROC_CONTEXTS, &a) != val) FAIL("COPROC_CONTEXTS");
 
-#ifdef HAVE_HLX
 	val = H2K_kg.hlx_contexts;
 	if (H2K_trap_info(INFO_HLX_CONTEXTS, &a) != val) FAIL("HLX_CONTEXTS");
-#endif
 
 #ifdef DO_EXT_SWITCH
 	val = a.vmblock->do_ext;

75f0d6fdc509fe4931d5bec03ecfba3df9665453
Andrey Karpenko <andreyk@qti.qualcomm.com>
2026-01-18 06:25:31 -0800

    Initial version of syscall wrapper for Picolibc
    Signed-off-by: Andrey Karpenko <andreyk@qti.qualcomm.com>

---
+++ libs/syscall/wrapper/src/syscall_wrapper.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#ifndef H2_SYSCALL_WRAPPER
+#define H2_SYSCALL_WRAPPER
+
+#define __machine_mbstate_t_defined
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/unistd.h>
+#include <sys/times.h>
+
+#endif /*H2_SYSCALL_WRAPPER*/

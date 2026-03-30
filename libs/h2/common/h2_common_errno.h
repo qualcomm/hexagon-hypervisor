5a2f687391e1a27453e909a297766cd7566fa3a3
Karpenko, Andrey <andreyk@qti.qualcomm.com>
2026-03-30 12:25:33 +0300

    Merge pull request #19 from Q6Auto/pthread_fix
    Add pthread_rwlock stubs
    
    (cherry picked from commit 043391f4a3849fc9c9c8fe73d6a3b61034c266c9)
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

---
+++ libs/h2/common/h2_common_errno.h
@@ -0,0 +1,31 @@
+#ifndef H2_COMMON_ERRNO_H
+#define H2_COMMON_ERRNO_H 1
+
+// values from dinkumware
+#define ENOENT        0x02
+#define EFAULT        0x0e
+#define ENFILE        0x17
+#define ENOTTY        0x19
+#define EFBIG         0x1b
+#define ENOSPC        0x1c
+#define ESPIPE        0x1d
+#define EROFS         0x1e
+#define EMLINK        0x1f
+#define EPIPE         0x20
+#define EDOM          0x21
+#define ERANGE        0x22
+#define EDEADLK       0x2d
+#define ENOLCK        0x2e
+#define ECANCELED     0x2f
+#define ENOTSUP       0x30
+#define EBADMSG       0x4d
+#define ENAMETOOLONG  0x4e
+#define EILSEQ        0x58
+#define ENOSYS        0x59
+#define ENOTEMPTY     0x5d
+#define EMSGSIZE      0x61
+#define ETIMEDOUT     0x91
+#define EINPROGRESS   0x96
+#define EFPOS         0x98
+
+#endif

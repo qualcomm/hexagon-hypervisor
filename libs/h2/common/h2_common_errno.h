afc209480a803aba69cf6e079233a2b69fe00267
Bryan Bayerdorffer <bryanb@qti.qualcomm.com>
2026-04-06 11:22:44 -0400

    Add missing copyright headers. (#5)
    Signed-off-by: Bryan Bayerdorffer <bryanb@qti.qualcomm.com>

--- libs/h2/common/h2_common_errno.h
+++ libs/h2/common/h2_common_errno.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
 #ifndef H2_COMMON_ERRNO_H
 #define H2_COMMON_ERRNO_H 1
 

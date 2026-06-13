9f6633700ffd2e53b8aa838e1c7f04c0e30d3289
zbelinsk <zbelinsk@qti.qualcomm.com>
2026-06-14 02:15:52 +0300

    Update globals.h
    Signed-off-by: zbelinsk <zbelinsk@qti.qualcomm.com>

--- kernel/data/globals/globals.h
+++ kernel/data/globals/globals.h
@@ -217,6 +217,11 @@ static inline H2K_kg_t PURITY *H2K_gp_llvm()
 #endif
 
 #ifdef CLUSTER_SCHED
+
+static inline u32_t H2K_hthread_cluster(u32_t hthread) {
+	return (hthread / H2K_gp->cluster_hthreads);
+}
+
 static inline void xex(u32_t hthread, u32_t new_xe, u32_t new_xe2, u32_t new_xe3, u32_t cur_xe, u32_t cur_xe2, u32_t cur_xe3) {
 #ifdef H2K_LOG_H
 	H2K_log("xex: new_xe %d  new_xe2 %d  new_xe3 %d  cur_xe %d  cur_xe2 %d  cur_xe3 %d\n", new_xe, new_xe2, new_xe3, cur_xe, cur_xe2, cur_xe3);

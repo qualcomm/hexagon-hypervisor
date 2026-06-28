4b59548d2aca82c3ec743bcfcb943d0e6182e1b1
Zeev Belinsky <zbelinsk@qti.qualcomm.com>
2026-06-28 06:03:39 -0700

    h2_compat/error: fix test to use trap1(#7) instead of SSR read
    The real issue this test exposed: the booter runs as supervisor guest
    (ssr_um=0, gm=1), so threads it creates inherit ssr_um=0. This causes
    these instructions to have unexpected behaviour:
    - SSR read: works cleanly
    - syscfg write: hangs
    
    The correct long-term fix is to make BOOT_THREAD_SSR include SSR_UM_BIT
    so the booter and its guest VMs run as true user-mode guests. However:
    - Kernel-level tests (e.g. kernel/util/stmode/test) run at monitor
      privilege reusing BOOT_THREAD_SSR, and would hang before reaching
      main() with ssr_um=1.
    
    For now, use trap1(#7) which unconditionally routes to H2K_vmtrap_bad
    -> H2K_vm_event(cause=0x1B) regardless of ssr_um, fixing the test.
    
    Signed-off-by: Zeev Belinsky <zbelinsk@qti.qualcomm.com>

--- libs/h2_compat/error/test/simple/test.c
+++ libs/h2_compat/error/test/simple/test.c
@@ -4,20 +4,21 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <h2.h>
 
 /* FIXME: need to check that we actually dumped the right state.  Also need to check h2_handle_errors(1), i.e. check that we call exit(). */
 
 int main() {
 
-	unsigned int x;
+	unsigned int ssr;
+	asm volatile ("%0 = ssr \n" : "=r"(ssr));
+	printf("ssr=0x%08x ssr_um=%d\n", ssr, (ssr >> 16) & 1);
 
-	//	h2_handle_errors(0) should be set by default
+	// h2_handle_errors(0);
 
-	// do something bad
-	//	*p++;
-	asm volatile ("%0 = ssr \n" : "=r"(x));
+	// do something bad - trap1 with invalid number triggers H2K_vmtrap_bad -> H2K_vm_event
+	printf("about to do bad trap\n");
+	asm volatile ("trap1(#7) \n");
 
 	/* Shouldn't reach here */
 	puts("FAIL\n");
